1ddd0ed57a2efadda4487fb3009f3b2a45db2f19
S <s>
2023-09-28 17:59:21 +0400

    {"types":"C","runs":16}

--- problem01.java
+++ problem01.java
@@ -4,6 +4,13 @@ import stanford.karel.Karel;
 //გადაიტანოს 4x1 უჯრაზე და დაამთავროს მუშაობა.
 public class problem01 extends Karel{
 	public void run(){
+		
+		/*
+		 * pre:karel is at 1x1, facing east, beeper at 3x1
+		 * 
+		 * 
+		 *
+		 */
 		move();
 		move();
 		pickBeeper();
