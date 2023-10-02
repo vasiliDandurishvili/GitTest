e2de9ed4d7036107c815c4d42341bd117bd760dc
S <s>
2023-10-02 17:46:44 +0400

    {"types":"C","runs":4}

--- problem_06.java
+++ problem_06.java
@@ -1,4 +1,22 @@
+import stanford.karel.Karel;
+/*
+*კარელმა უნდა შეკრიბოს ორი რიცხვი, 2x1 უჯრაზე დევს n ცალი ბრილიანტი, 3x1 უჯრაზე m
+ცალი ბრილიანტი, კარელმა 4x1 უჯრაზე უნდა დადოს n+m ბრილიანტი. ჩათვალეთ, რომ
+კარელს ჩანთაში უსასრულო რაოდენობის ბრილიანტი აქვს.
+*/
 
-public class problem_06 {
+public class problem_06 extends Karel{	
+
+	public void run(){
+		while (frontIsClear()){
+			if(beepersPresent()){
+				pickBeeper();
+			}else {
+				move();
+			}
+			
+		}
+
+	}
 
 }
