f26cb2be808ef7d7bb0dfe6a22b01ce3a6370c2a
S <s>
2023-10-03 17:16:57 +0400

    {"types":"C","runs":17}

--- prolem_02CLASS.java
+++ prolem_02CLASS.java
@@ -1,4 +1,32 @@
+import stanford.karel.Karel;
 
-public class prolem_02CLASS {
+/*კარელი დგას 1x1 უჯრაზე. 5x1 უჯრაზე დევს 10 ცალი ბრილიანტი, კარელმა ათივე
+ბრილიანტი უნდა გადაიტანოს 6x1 უჯრაზე.
+ */
+public class prolem_02CLASS extends Karel{
+	public void run(){
+		moveToTheBeeperPile();
+		pickTenBeepers();
+		move();
+		putTenBeepers();
+		
+	}
 
+	private void putTenBeepers() {
+		for (int i=0; i<4;i++){
+			move();
+		}
+		
+		
+	}
+
+	private void pickTenBeepers() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void moveToTheBeeperPile() {
+		// TODO Auto-generated method stub
+		
+	}
 }
