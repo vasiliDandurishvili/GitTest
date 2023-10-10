c9ece9bfc27e218a1868f682ea1a973d4871055c
S <s>
2023-10-10 17:24:42 +0400

    {"types":"C","runs":38}

--- Problem_12.java
+++ Problem_12.java
@@ -4,7 +4,7 @@ import acm.program.GraphicsProgram;
 public class Problem_12 extends GraphicsProgram {
 	
 	public void run() {
-		add(new GOval(10,20));
+		add(new GOval(100, 200),100,100);
 		
 	}
 }
