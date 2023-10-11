a91ad3a82fdb877c3c2d0e5f281097b313276316
S <s>
2023-10-11 19:26:11 +0400

    {"types":"C","runs":50}

--- assigment_3.java
+++ assigment_3.java
@@ -5,6 +5,18 @@ public class assigment_3 extends SuperKarel{
 	}
 	private void fillTheBoard() {
 		fillLine();
+		goBackAndMoveToTheNextLine();
+	}
+	private void goBackAndMoveToTheNextLine() {
+		if(frontIsBlocked()){
+			turnAround();
+			while(frontIsClear()){
+				move();
+			}
+			turnRight();
+			move();
+			turnRight();
+		}
 	}
 	private void fillLine() {
 			putBeeper();
