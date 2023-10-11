7dbddc38de2c6bd0151bb2b8558ccabddbe9b924
S <s>
2023-10-11 19:53:42 +0400

    {"types":"C","runs":55}

--- prolem_12.java
+++ prolem_12.java
@@ -2,9 +2,9 @@ import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 
 public class prolem_12 extends GraphicsProgram {
-	
 	public void run() {
 		GRect myRect = new GRect (200,100);
+		add(myRect,400,500);
 	}
 
 }
