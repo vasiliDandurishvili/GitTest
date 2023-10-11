d80d5efbb90d682165908e592cd73035f95e5399
S <s>
2023-10-11 22:37:03 +0400

    {"types":"C","runs":84}

--- Problem_12.java
+++ Problem_12.java
@@ -1,6 +1,6 @@
 import java.awt.Color;
 import acm.graphics.GPolygon;
-import acm.graphics.GRect;
+import acm.graphics.*;
 import acm.program.GraphicsProgram;
 
 public class Problem_12 extends GraphicsProgram {
@@ -11,6 +11,7 @@ public class Problem_12 extends GraphicsProgram {
 		myWall.setFilled(true);
 		myWall.setColor(Color.BLUE);
 		
+		GLine Roof_1 = new GLine (600,400,1000,200);
 		
 
 	}
