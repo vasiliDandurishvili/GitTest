db7ba8fdc308f10b68658a1f9153c1e235d51871
S <s>
2023-10-11 23:12:58 +0400

    {"types":"C","runs":104}

--- Problem_12.java
+++ Problem_12.java
@@ -39,6 +39,9 @@ public class Problem_12 extends GraphicsProgram {
 		
 		GLine Wline2_2 = new GLine (1100,575,1250,575);
 		add(Wline2_2);
+		
+		GRect myDoor = new GRect(50,150);
+		add(myDoor,975,650);
 	}
 
 }
