b37b2d07ed7a88d1c41a6c2f9928e6218b2f2554
S <s>
2023-10-11 20:07:11 +0400

    {"types":"C","runs":78}

--- Problem_12.java
+++ Problem_12.java
@@ -4,11 +4,11 @@ import acm.program.GraphicsProgram;
 
 public class Problem_12 extends GraphicsProgram {
 	public void run() {
-		GRect myRect = new GRect(800,400);
-		add(myRect,600,400);
-		
-		myRect.setFilled(true);
-		myRect.setColor(Color.BLUE);
+//		GRect myRect = new GRect(800,400);
+//		add(myRect,600,400);
+//		
+//		myRect.setFilled(true);
+//		myRect.setColor(Color.BLUE);
 		
 		GRect myTria = new GRect(800,400);
 		add(myTria,600,400);
