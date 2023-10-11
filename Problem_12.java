cd0219b2cc16ddabecf41a7f4b96ba1f59de07f0
S <s>
2023-10-11 19:56:52 +0400

    {"types":"C","runs":58}

--- Problem_12.java
+++ Problem_12.java
@@ -1,10 +1,14 @@
+import acm.graphics.GOval;
 import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 
 public class Problem_12 extends GraphicsProgram {
 	public void run() {
-		GRect myRect = new GRect (200,100);
+		GRect myRect = new GRect(200,100);
 		add(myRect,400,500);
 	}
 
 }
+//GOval myOval = new GOval(100, 200);
+//add(myOval,100,100);
+////GOval secondOval = new GOval(100, 200);
