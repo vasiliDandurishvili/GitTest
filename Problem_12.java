f8823b26190b3d07ddb91b0a8a4451fff1775b90
S <s>
2023-10-11 19:55:21 +0400

    {"types":"AR","runs":56}

---
+++ Problem_12.java
@@ -0,0 +1,10 @@
+import acm.graphics.GRect;
+import acm.program.GraphicsProgram;
+
+public class Problem_12 extends GraphicsProgram {
+	public void run() {
+		GRect myRect = new GRect (200,100);
+		add(myRect,400,500);
+	}
+
+}
