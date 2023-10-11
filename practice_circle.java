6b4c561bbba60af8781aef76e05325b651b142f1
S <s>
2023-10-11 19:40:02 +0400

    {"types":"A","runs":55}

---
+++ practice_circle.java
@@ -0,0 +1,27 @@
+import java.awt.Color;
+
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
+
+public class practice_circle extends GraphicsProgram {
+	
+	public void run() {
+		GOval myOval = new GOval(100, 200);
+		add(myOval,100,100);
+//		GOval secondOval = new GOval(100, 200);
+		
+		myOval =new GOval(100, 200);
+		add(myOval ,200,200);
+		
+		
+		GOval oldBall = myOval;
+		
+		myOval = new GOval(100,200,200,300);
+		add(myOval);
+		
+		myOval.setFilled(true);
+		myOval.setColor(Color.RED);
+		
+		oldBall.setFilled(true);
+	}
+}
