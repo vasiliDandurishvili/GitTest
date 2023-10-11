2bc34e0d75ced5a70cc99b3a549bff93e4e5dcbf
S <s>
2023-10-11 19:58:00 +0400

    {"types":"C","runs":59}

--- practice_circle.java
+++ practice_circle.java
@@ -9,19 +9,19 @@ public class practice_circle extends GraphicsProgram {
 		GOval myOval = new GOval(100, 200);
 		add(myOval,100,100);
 //		GOval secondOval = new GOval(100, 200);
-		
-		myOval =new GOval(100, 200);
-		add(myOval ,200,200);
-		
-		
-		GOval oldBall = myOval;
-		
-		myOval = new GOval(100,200,200,300);
-		add(myOval);
-		
-		myOval.setFilled(true);
-		myOval.setColor(Color.RED);
-		
-		oldBall.setFilled(true);
-	}
+//		
+//		myOval =new GOval(100, 200);
+//		add(myOval ,200,200);
+//		
+//		
+//		GOval oldBall = myOval;
+//		
+//		myOval = new GOval(100,200,200,300);
+//		add(myOval);
+//		
+//		myOval.setFilled(true);
+//		myOval.setColor(Color.RED);
+//		
+//		oldBall.setFilled(true);
+//	}
 }
