772d6255e20161cdaad05db8fe76d0006a07bc7c
S <s>
2023-10-12 17:15:28 +0400

    {"types":"C","runs":124}

--- Class_Practice_1.java
+++ Class_Practice_1.java
@@ -1,4 +1,30 @@
+import java.awt.Color;
+import acm.graphics.GPolygon;
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
 
-public class Class_Practice_1 {
-
+public class Class_Practice_1 extends GraphicsProgram {
+	public void run() {
+		int a = 5;
+		a=a + 5;
+		
+		if(2 == 2){
+			double b = 3;
+			b = 4;
+			b = a + 3;
+			
+			
+//			int a = 7 mistake
+			GLabel label = new GLabel("a:" + a);
+			a = 3;
+		}
+		
+		b = b + 1
+	}	
+	
+	
+	private void testVoid(){
+		int a = 7;
+		
+	}
 }
