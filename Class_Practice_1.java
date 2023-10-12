9392808f517b54c046dd37e227eeb2317571235d
S <s>
2023-10-12 17:39:42 +0400

    {"types":"C","runs":129}

--- Class_Practice_1.java
+++ Class_Practice_1.java
@@ -4,12 +4,32 @@ import acm.graphics.*;
 import acm.program.GraphicsProgram;
 
 public class Class_Practice_1 extends GraphicsProgram {
+//	private static final double PI = 3.14;
+	private static final double HOUSE_WIDTH = 200;
+	private static final double HOUSE_HEIGHT = 300;
+	private static final double DOOR_WIDTH = 40;
+	private static final double DOOR_HEIGHT = 60;
+	
 	public void run() {
+//		PI = 3;
 		drawMidLine();
-		
+		drawHouse();
 		
 	}
-
+	private void drawHouse() {
+		drawWall();
+		drawRoof();
+		drawDoor();
+		drawWindowa();
+		
+	}
+	private void drawWall() {
+		GRect wall = new GRect (HOUSE_WIDTH, HOUSE_HEIGHT);
+		int x = (int) (getWidth() / 2 - HOUSE_WIDTH) ;
+		double y = getHeight()-HOUSE_HEIGHT;
+		add(wall, x, y);
+		
+	}
 	private void drawMidLine() {
 		double x1 = 0;
 		double y1 = getHeight() / 2;
