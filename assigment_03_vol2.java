656295553bc8327130b65389c1585dc793d83fa4
S <s>
2023-10-14 13:25:53 +0400

    {"types":"C","runs":6}

--- assigment_03_vol2.java
+++ assigment_03_vol2.java
@@ -10,7 +10,22 @@ public class assigment_03_vol2 extends SuperKarel{
 this is a main function which contains all small and big steps.	  
  */
 	private void makeChessBoard() {
-		
+		if (frontIsClear()){
+			fillHorizontals();
+		}else{
+//			fillVertical();
+		}
+	}
+	
+	private void fillHorizontals() {
+		putBeeper();
+		while (frontIsClear()){
+			move();
+			if(frontIsClear()){
+				move();
+				putBeeper();
+			}
+		}
 		
 	}
 }
