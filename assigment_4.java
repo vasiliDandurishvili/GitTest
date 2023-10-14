761321c27ba656a59cc658d75078e46e438f5949
S <s>
2023-10-14 16:02:14 +0400

    {"types":"C","runs":1}

--- assigment_4.java
+++ assigment_4.java
@@ -15,6 +15,12 @@ this is a main method which contains all small and big steps.
 			doubleMoveUpAndOneRight();
 		}
 	}
+/*
+this is a cycle to reach middle vertical line by moving up twice and once right,
+When Karel passes X above, he will have passed nearly X/2 to the right and
+when it will climb to the top it will go back to the bottom and stand in the
+middle of the first horizontal and puts beeper.
+*/
 	private void doubleMoveUpAndOneRight() {
 		while (frontIsClear()){
 			move();
@@ -30,6 +36,9 @@ this is a main method which contains all small and big steps.
 			}
 		}
 	}
+/*
+this is a method to move karel from top to the bottom.	  
+*/
 	private void goDown() {
 		turnAround();
 		while(frontIsClear()){
