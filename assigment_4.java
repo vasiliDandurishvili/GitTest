dd46f87b1bc26984a9eec94637b4d394f95e493a
S <s>
2023-10-14 15:35:00 +0400

    {"types":"C","runs":23}

--- assigment_4.java
+++ assigment_4.java
@@ -2,6 +2,30 @@ import stanford.karel.SuperKarel;
 
 public class assigment_4 extends SuperKarel{
 	public void run(){
+		putBeeperInTheMiddle();
+	}
+/*
+this is a main method which contains all small and big steps.	  
+*/
+	private void putBeeperInTheMiddle() {
+		if (frontIsBlocked()){
+			putBeeper();
+		}else{
+			turnLeft();
+			doubleMoveUpAndOneRight();
+		}
+	}
+	private void doubleMoveUpAndOneRight() {
+		while (frontIsClear()){
+			move();
+			if (frontIsClear()){
+				move();
+				turnRight();
+				move();
+				turnLeft();
+			}
+		}
+		
 		
 	}
 }
