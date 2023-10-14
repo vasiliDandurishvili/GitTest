37a1c5fcfd08655e469449cc8245dcc53ac3d970
S <s>
2023-10-14 14:35:27 +0400

    {"types":"C","runs":21}

--- assigment_3.java
+++ assigment_3.java
@@ -1,36 +1,37 @@
 import stanford.karel.SuperKarel;
-/*
-  
- */
+
 public class assigment_3 extends SuperKarel{
 	public void run(){
 		makeChessBoard();
 	}
 /*
-this is a main function which contains all small and big steps.	  
+this is a main method which contains all small and big steps.	  
  */
 	private void makeChessBoard() {
 		if (frontIsClear()){
 			fillHorizontals();
 		}else{
-//			fillVertical();
+			fillVertical();
 		}
 	}
-	
+/*
+this is a cycle to fill the one horizontal and   move to the
+ second again and again Until we climb to the top. 
+ */	
 	private void fillHorizontals() {
 		putBeeper();
 		while (frontIsClear()){
 			move();
-			move();
-			putBeeper();
+			if(frontIsClear()){
+				move();
+				putBeeper();
+			}
 			if(frontIsBlocked()){
 				goBackAndMoveToTheNextLine();
-			}
-
-			
+			}	
 		}
-		
 	}
+	
 	private void goBackAndMoveToTheNextLine(){
 		turnAround();
 		while(frontIsClear()){
@@ -39,8 +40,8 @@ this is a main function which contains all small and big steps.
 		if(rightIsClear()){
 			moveToTheNextLine();
 		}
-		
 	}
+	
 	private void moveToTheNextLine() {
 		if(beepersPresent()){
 			turnRight();
@@ -53,6 +54,17 @@ this is a main function which contains all small and big steps.
 			move();
 			turnRight();
 			putBeeper();
+		}
+	}
+	private void fillVertical() {
+		turnLeft();
+		putBeeper();
+		while (frontIsClear()){
+			move();
+			if(frontIsClear()){
+				move();
+				putBeeper();
+			}
 		}
 	}
 }
