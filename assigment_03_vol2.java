9ea654f7df779b0430de2fad65e069d37101aa79
S <s>
2023-10-14 13:00:59 +0400

    {"types":"C","runs":2}

--- assigment_03_vol2.java
+++ assigment_03_vol2.java
@@ -4,42 +4,6 @@ import stanford.karel.SuperKarel;
  */
 public class assigment_03_vol2 extends SuperKarel{
 	public void run(){
-		fillOneBoard();
-		completeTheBoard();
-	}
-
-	private void completeTheBoard() {
-		while(frontIsClear()){
-			move();
-		}
-		turnAround();
-	}
-
-	private void fillOneBoard() {
-		putBeeper();
-		while(frontIsClear()){
-			if(noBeepersPresent()){
-				putBeeper();
-			}
-			move();
-			if(frontIsClear()){
-				move();
-				putBeeper();
-			}
-		}
-		turnAround();
-		while(frontIsClear()){
-			move();
-		}
-		turnRight();
-		move();
-		turnRight();
-//		if(frontIsClear()){
-//			move();
-//			putBeeper();
-//		}
 		
 	}
-	
-		
 }
