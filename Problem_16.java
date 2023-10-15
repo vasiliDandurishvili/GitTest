6a0b90a256b4493428ce10d6b3ea2800fb79bcab
S <s>
2023-10-15 22:06:23 +0400

    {"types":"C","runs":11}

--- Problem_16.java
+++ Problem_16.java
@@ -1,4 +1,16 @@
+import acm.program.ConsoleProgram;
 
-public class Problem_16 {
+public class Problem_16 extends ConsoleProgram {
 
+	public void run(){
+		println("this program gives us three numbers's harmonic.");
+		int n1 = readInt ("enter n1: ");
+		int n2 = readInt ("enter n2: ");
+		int n3 = readInt ("enter n3: ");
+		double harmonic = (double) 1 / (1 / n1 + 1 / n2 + 1 / n3);
+		println("three integers harmonic: "+ harmonic + ".");
+		
+		
+		
+	}
 }
