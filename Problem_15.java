5ef501dce1a95512463a9b8a70eb1cfa28c0e09f
S <s>
2023-10-15 21:59:29 +0400

    {"types":"C","runs":8}

--- Problem_15.java
+++ Problem_15.java
@@ -3,10 +3,21 @@ import acm.program.*;
 public class Problem_15 extends ConsoleProgram {
 
 	public void run(){
-		println("this program adds two numbers.");
+		println("this program gives us two numbers's middle.");
 		int n1 = readInt ("enter n1: ");
 		int n2 = readInt ("enter n2: ");
-		int total = n1 + n2;
-		println("the totalis" + total + ".");
+		double middle = (n1 + n2) / 2;
+		println("this is two integers middle: " + middle + ".");
 	}
 }
+
+
+
+
+//public void run(){
+//	println("this program adds two numbers.");
+//	int n1 = readInt ("enter n1: ");
+//	int n2 = readInt ("enter n2: ");
+//	int total = n1 + n2;
+//	println("the totalis" + total + ".");
+//}
