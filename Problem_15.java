6a54023bfa210c3eca5b63a0fefb88243cef9995
S <s>
2023-10-15 21:54:56 +0400

    {"types":"C","runs":7}

--- Problem_15.java
+++ Problem_15.java
@@ -4,8 +4,9 @@ public class Problem_15 extends ConsoleProgram {
 
 	public void run(){
 		println("this program adds two numbers.");
-		int n1 = readint ("enter n1: ");
-		
-		
+		int n1 = readInt ("enter n1: ");
+		int n2 = readInt ("enter n2: ");
+		int total = n1 + n2;
+		println("the total")
 	}
 }
