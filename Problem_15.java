ee6706d5dabee3e5f314e12963b46eb084b5bc6a
S <s>
2023-10-15 21:55:12 +0400

    {"types":"C","runs":7}

--- Problem_15.java
+++ Problem_15.java
@@ -7,6 +7,6 @@ public class Problem_15 extends ConsoleProgram {
 		int n1 = readInt ("enter n1: ");
 		int n2 = readInt ("enter n2: ");
 		int total = n1 + n2;
-		println("the total")
+		println("the totalis" + total + ".");
 	}
 }
