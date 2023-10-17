1bf521b73d7725460209c9f271fbe22a56b9ec2b
S <s>
2023-10-17 15:57:11 +0400

    {"types":"C","runs":6}

--- Problem_17.java
+++ Problem_17.java
@@ -5,7 +5,7 @@ public class Problem_17 extends ConsoleProgram {
 	public void run(){
 		double x = readDouble("enter your nymber: ");
 		println((int) x);
-		println(x - (double) x);
+		println(x - (int) x);
 		
 	}
 }
