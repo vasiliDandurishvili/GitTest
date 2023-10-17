9cbf200bb94db049c1f4ffde65f16fdfb0d4800a
S <s>
2023-10-17 16:50:35 +0400

    {"types":"C","runs":27}

--- Problem_20.java
+++ Problem_20.java
@@ -5,7 +5,7 @@ public class Problem_20 extends ConsoleProgram {
 	public void run(){
 		int i = 0;
 		int x = readInt ("enter your number: ");
-		for (; i <= x; i++ ){
+		for (; i < x; i++ ){
 			i += i;
 		}
 		println("add integers until " + x + ": " + i );
