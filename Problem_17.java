4ddb4f6b3309548a1cc94d15ae0ae053e38ffc55
S <s>
2023-10-17 16:01:23 +0400

    {"types":"C","runs":8}

--- Problem_17.java
+++ Problem_17.java
@@ -3,8 +3,8 @@ import acm.program.ConsoleProgram;
 public class Problem_17 extends ConsoleProgram {
 
 	public void run(){
-		double x = readDouble("enter your nymber: ");
-		println((int) x);
-		println(x - (int) x);
+		double x = readDouble("enter your number: ");
+		println("print integer: " + (int) x);
+		println("print double without integer:" + (x - (int) x));
 	}
 }
