fe5bd1747c9dd51f79cac6b133257135e79c9dcf
S <s>
2023-10-17 16:44:55 +0400

    {"types":"C","runs":23}

--- Problem_20.java
+++ Problem_20.java
@@ -3,6 +3,12 @@ import acm.program.ConsoleProgram;
 public class Problem_20 extends ConsoleProgram {
 
 	public void run(){
-		int x = 
+		int i = 0;
+		int x = readInt ("enter your number: ");
+		for (; i <= x; i++ ){
+			i += i;
+		}
+		println("add integers until" + x + ":" + i );
+		println(i);
 	}
 }
