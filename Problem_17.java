64ca776541d02002652046a8f9cd1ba7a4c34f18
S <s>
2023-10-17 15:55:30 +0400

    {"types":"C","runs":4}

--- Problem_17.java
+++ Problem_17.java
@@ -1,4 +1,11 @@
+import acm.program.ConsoleProgram;
 
-public class Problem_17 {
+public class Problem_17 extends ConsoleProgram {
 
+	public void run(){
+		double x = readDouble("enter your nymber: ");
+		println((int) x);
+		
+		
+	}
 }
