24d1661020aa93b4d119e7f669ce23a4e5b59957
S <s>
2023-10-18 22:09:54 +0400

    {"types":"C","runs":46}

--- Problem_20.java
+++ Problem_20.java
@@ -5,16 +5,12 @@ import acm.program.ConsoleProgram;
 public class Problem_20 extends ConsoleProgram {
 
 	public void run(){
-//		int x = readInt ("enter your number: ");
-//		for (int i = 0; i < x; i++){
-//			int a = readInt("enter " + (i+1) + "th integer: ");
-//			a += a;
-//			print(a);
-//	
-//		}
-		int a = 5;
-		int b = 6;
-		a += b;
-		print(a);
+		int x = readInt ("enter your number: ");
+		for (int i = 0; i < x; i++){
+			int a = readInt("enter " + (i+1) + "th integer: ");
+			a += a;
+			print(a);
+	
+		}
 	}
 }
