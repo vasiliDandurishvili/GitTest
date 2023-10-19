683bce223ec789a962c5b5ab97a9b4c845bcc090
S <s>
2023-10-19 17:54:15 +0400

    {"types":"C","runs":59}

--- Problem_22.java
+++ Problem_22.java
@@ -1,4 +1,12 @@
-
-public class Problem_22 {
-
+import acm.program.GraphicsProgram;
+import acm.graphics.*;
+/*
+ * ხაზებით ბადის დახატვა 10x10 ზე.
+ */
+public class Problem_22 extends GraphicsProgram {
+	private static final int NUM_LINES = 11;
+	public void run() {
+		
+		
+	}
 }
