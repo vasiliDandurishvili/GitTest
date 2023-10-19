abcad4b0bd2b96d940fd200852867ae644b483b8
S <s>
2023-10-19 17:48:32 +0400

    {"types":"C","runs":56}

--- Problem_21.java
+++ Problem_21.java
@@ -3,9 +3,16 @@ import acm.program.GraphicsProgram;
  * ხაზებით ბადის დახატვა 10x10 ზე.
  */
 public class Problem_21 extends GraphicsProgram {
-	
+	private static final int NUM_LINES = 11;
 	public void run() {
-		
+		for(int i =0; i < NUM_LINES; i++){
+		drawHorizontalLine(i);
+		}
 	}
-
+	private void drawHorizontalLine(int i) {
+		double y = i * (getHeight() / (NUM_LINES -1.0));
+		double x_1 = 0;
+		double x_2 = getWidth();
+		Gline line = new Gline (x_1, y, x_2, y);
+		}
 }
