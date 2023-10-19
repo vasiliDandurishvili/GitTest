258d9c68b31c65b71f43d23bc722e7abb5775d4a
S <s>
2023-10-19 17:37:35 +0400

    {"types":"C","runs":56}

--- Problem_21.java
+++ Problem_21.java
@@ -1,9 +1,11 @@
 import acm.program.GraphicsProgram;
-
+/*
+ * ხაზებით ბადის დახატვა 10x10 ზე.
+ */
 public class Problem_21 extends GraphicsProgram {
 	
 	public void run() {
-		// You fill this in
+		
 	}
 
 }
