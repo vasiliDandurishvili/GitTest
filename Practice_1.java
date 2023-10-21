2715e18fcaee7182a6134a5c350c6c482ac9df65
S <s>
2023-10-21 12:48:33 +0400

    {"types":"C","runs":6}

--- Practice_1.java
+++ Practice_1.java
@@ -1,4 +1,20 @@
+import java.math.BigDecimal;
 
-public class Practice_1 {
+import acm.program.ConsoleProgram;
 
+public class Practice_1 extends ConsoleProgram {
+	public void run () {
+		calculateNumInBigDecimal();
+		
+	}
+
+	private void calculateNumInBigDecimal() {
+		double a = readDouble ("enter real number: " );
+		String a_AsString = "" + a;
+		BigDecimal decimal = new BigDecimal (a_AsString);
+		
+		int integerPart = decimal.intValue() ;
+		BigDecimal fractionPart = decimal.remainder(BigDecimal.ONE);
+		
+	}
 }
