ff6a0854950ba84b70f5cb98a3164ebb80d598ee
S <s>
2023-10-21 16:34:22 +0400

    {"types":"C","runs":33}

--- Problem_25.java
+++ Problem_25.java
@@ -5,18 +5,23 @@ import acm.program.ConsoleProgram;
  */
 public class Problem_25 extends ConsoleProgram {
 	public void run () {
-		int num_1 = readInt("enter first the integer:");
-		int num_2 = readInt("enter second the integer:");
-		for (int i = 1; i <= num_1; i++){
-			boolean firstsDividers = num_1 % i ==0;
-			if (firstsDividers){
-				
+		int num_1 = readInt("enter first integer:");
+		int num_2 = readInt("enter second integer:");
+		
+		int LeastCommonMultiple = num_1 * num_2;
+		for (int i = 1; i <= num_1;i++){
+			boolean divisionNum_1_WithoutRemainder = num_1 % i == 0;
+			if(divisionNum_1_WithoutRemainder){
+				for(int j = 1; j <= num_2; i++){
+					boolean divisionNum_2_WithoutRemainder = num_2 % j == 0;
+					if(divisionNum_2_WithoutRemainder){
+						if(i == j){
+							LeastCommonMultiple = LeastCommonMultiple / i;
+						}
+					}
+				}
 			}
 		}
-		
+		println("the least common multiple: " + LeastCommonMultiple);
 	}
-}	
-
-//if(a == b){
-//	continue
-//}
+}
