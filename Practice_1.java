9de1adad16970e07a2d58197f0b4b726cfcc2066
S <s>
2023-10-21 13:08:57 +0400

    {"types":"C","runs":6}

--- Practice_1.java
+++ Practice_1.java
@@ -14,7 +14,10 @@ public class Practice_1 extends ConsoleProgram {
 		BigDecimal decimal = new BigDecimal (a_AsString);
 		
 		int integerPart = decimal.intValue() ;
-		BigDecimal fractionPart = decimal.remainder(BigDecimal.ONE);
+		BigDecimal franctionPart = decimal.remainder(BigDecimal.ONE);
+		
+		println("Integer part: " + integerPart);
+		println("Franction part (as BigDecimal): " + franctionPart);
 		
 	}
 }
