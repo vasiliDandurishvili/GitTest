253a858da986de7761b7342d66c93fa30e5ede37
S <s>
2023-10-21 17:31:58 +0400

    {"types":"C","runs":51}

--- Problem_27.java
+++ Problem_27.java
@@ -1,4 +1,9 @@
-
-public class Problem_27 {
-
+import acm.program.ConsoleProgram;
+/*
+მომხმარებელს შეყავს n და m დაბეჭდეთ n^m
+ */
+public class Problem_27 extends ConsoleProgram {
+	public void run () {
+		
+	}
 }
