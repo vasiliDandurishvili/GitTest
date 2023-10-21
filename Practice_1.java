8a1295fcd321261b35a8327bde0f8df421d92dc6
S <s>
2023-10-21 16:22:31 +0400

    {"types":"C","runs":26}

--- Practice_1.java
+++ Practice_1.java
@@ -11,19 +11,14 @@ public class Practice_1 extends ConsoleProgram {
 		int num_2 = readInt("enter second integer:");
 		
 		int LeastCommonMultiple = num_1 * num_2;
-		println(1);
 		for (int i = 1; i <= num_1;i++){
-			println(2);
 			boolean divisionNum_1_WithoutRemainder = num_1 % i == 0;
-			println(3);
 			if(divisionNum_1_WithoutRemainder){
-				println(4);
 				for(int j = 1; j <= num_2; i++){
 					boolean divisionNum_2_WithoutRemainder = num_2 % j == 0;
 					if(divisionNum_2_WithoutRemainder){
 						if(i == j){
 							LeastCommonMultiple /= i;
-							println(6);
 						}
 					}
 							
@@ -31,6 +26,7 @@ public class Practice_1 extends ConsoleProgram {
 
 
 			}
+			println("the least common multiple: " + LeastCommonMultiple);
 		}
 		println("the least common multiple: " + LeastCommonMultiple);
 	}
