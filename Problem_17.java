fd478e2eb98943b26718b839759a77a38c3fc165
S <s>
2023-10-21 12:26:13 +0400

    {"types":"C","runs":0}

--- Problem_17.java
+++ Problem_17.java
@@ -7,23 +7,21 @@ public class Problem_17 extends ConsoleProgram {
 	public void run(){
 		double a = readDouble("enter your number: ");
 		calculateInDouble(a);
-		calculateInBigDecimal(a);
+		calculateInBigDecimal();
 	}
 	
 	
-	private void calculateInBigDecimal(double a) {
-/private void calculateInBigDecimal() {
-	double a = readDouble("Enter real number: ");
-	String aAsString = "" + a;
-	BigDecimal decimal = new BigDecimal(aAsString);
+	private void calculateInBigDecimal() {
+		double a = readDouble("Enter real number: ");
+		String aAsString = "" + a;
+		BigDecimal decimal = new BigDecimal(aAsString);
 	
-	int integerPart = decimal.intValue();
-	BigDecimal franctionPart = decimal.remainder(BigDecimal.ONE);
+		int integerPart = decimal.intValue();
+		BigDecimal franctionPart = decimal.remainder(BigDecimal.ONE);
 	
-	println("Integer part: " + integerPart);
-	println("Franction part (as BigDecimal): " + franctionPart);
+		println("Integer part: " + integerPart);
+		println("Franction part (as BigDecimal): " + franctionPart);
 	}
-}
 
 	private void calculateInDouble(double a){
 		int integerPart = (int)a;
