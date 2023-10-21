bab05d79b326b9bb8d9fc55ef0b97a70bcc36b37
S <s>
2023-10-21 13:24:06 +0400

    {"types":"C","runs":7}

--- Problem_21.java
+++ Problem_21.java
@@ -5,29 +5,61 @@ import acm.graphics.*;
  */
 public class Problem_21 extends GraphicsProgram {
 	private static final int NUM_LINES = 11;
-	
-	public void run() {
-		for(int i =0; i < NUM_LINES; i++){
-		drawHorizontalLine(i);
-		drawVerticalLine(i);
+	public void run(){
+		for (int i = 0; i < NUM_LINES; i++ ){
+			drawHorizonalLIne();
+//			drawVerticalLIne();
 		}
-	}
-	private void drawVerticalLine(int i) {
-		double x = i * (getWidth() / (NUM_LINES -1.0));
-		double y_1 = 0;
-		double y_2 = getHeight();
 		
-		GLine line = new GLine (x, y_1, x, y_2);
-		add(line);
+		
+		
+	}
+	private void drawHorizonalLIne() {
+		double x_1 = i * (getHeight / (NUM_LINES - 1.0));
 		
 	}
 	
-	private void drawHorizontalLine(int i) {
-		double y = i * (getHeight() / (NUM_LINES -1.0));
-		double x_1 = 0;
-		double x_2 = getWidth();
-		
-		GLine line = new GLine (x_1, y, x_2, y);
-		add(line);
-		}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//	private static final int NUM_LINES = 11;
+//	
+//	public void run() {
+//		for(int i =0; i < NUM_LINES; i++){
+//		drawHorizontalLine(i);
+//		drawVerticalLine(i);
+//		}
+//	}
+//	private void drawVerticalLine(int i) {
+//		double x = i * (getWidth() / (NUM_LINES -1.0));
+//		double y_1 = 0;
+//		double y_2 = getHeight();
+//		
+//		GLine line = new GLine (x, y_1, x, y_2);
+//		add(line);
+//		
+//	}
+//	
+//	private void drawHorizontalLine(int i) {
+//		double y = i * (getHeight() / (NUM_LINES -1.0));
+//		double x_1 = 0;
+//		double x_2 = getWidth();
+//		
+//		GLine line = new GLine (x_1, y, x_2, y);
+//		add(line);
+//		}
 }
