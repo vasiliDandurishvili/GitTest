b0372285883cbb9521aae440b5a6a596e715d81d
S <s>
2023-10-21 16:24:41 +0400

    {"types":"C","runs":28}

--- Practice_1.java
+++ Practice_1.java
@@ -17,15 +17,13 @@ public class Practice_1 extends ConsoleProgram {
 				for(int j = 1; j <= num_2; i++){
 					boolean divisionNum_2_WithoutRemainder = num_2 % j == 0;
 					if(divisionNum_2_WithoutRemainder){
-						
+						if(i == j){
 							LeastCommonMultiple /= i;
 							println("the least common multiple: " + LeastCommonMultiple);
-						
+						}
 					}
 							
 				}
-
-
 			}
 		}
 	}
