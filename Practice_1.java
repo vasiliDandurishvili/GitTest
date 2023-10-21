02780f2ba2f742321798832dc05e16d1cdf702fd
S <s>
2023-10-21 16:17:06 +0400

    {"types":"C","runs":21}

--- Practice_1.java
+++ Practice_1.java
@@ -1,16 +1,29 @@
 import java.math.BigDecimal;
-
 import acm.program.ConsoleProgram;
-
+/*
+ მომხმარებელს შეჰყავს ორი რიცხვი თქვენმა პროგრამამ უნდა დაბეჭდოს ამ ორი რიცხვის
+უმცირესი საერთო ჯერადი.
+ */
 public class Practice_1 extends ConsoleProgram {
 	public void run () {
 //		task 25
 		int num_1 = readInt("enter first integer:");
 		int num_2 = readInt("enter second integer:");
+		
+		int LeastCommonMultiple = num_1 * num_2;
+		
 		for (int i = 1; i <= num_1;i++){
-			boolean a = num_1 % i == 0;
-			if(a){
-				
+			boolean divisionNum_1_WithoutRemainder = num_1 % i == 0;
+			if(divisionNum_1_WithoutRemainder){
+				for(int j = 1; j <= num_2; i++){
+					boolean divisionNum_2_WithoutRemainder = num_2 % j == 0;
+					if(divisionNum_2_WithoutRemainder){
+						if(i == j){
+							LeastCommonMultiple /= i; 
+						}
+					}
+							
+				}
 
 
 			}
