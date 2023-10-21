03ac37c8a67c8f4a0d08cce94c17463ad367f3b3
S <s>
2023-10-21 16:05:15 +0400

    {"types":"C","runs":18}

--- Practice_1.java
+++ Practice_1.java
@@ -4,21 +4,47 @@ import acm.program.ConsoleProgram;
 
 public class Practice_1 extends ConsoleProgram {
 	public void run () {
-//		task 17
-		calculateNumInBigDecimal();
+//		task 25
+		int num_1 = readInt("enter first the integer:");
+		for (int i = 1; i <= num_1;i++){
+			boolean a = num_1 % i == 0;
+			if(a){
+				println(a);
+			}
+		}
 		
-	}
-
-	private void calculateNumInBigDecimal() {
-		double a = readDouble ("enter real number: " );
-		String a_AsString = "" + a;
-		BigDecimal decimal = new BigDecimal (a_AsString);
 		
-		int integerPart = decimal.intValue() ;
-		BigDecimal franctionPart = decimal.remainder(BigDecimal.ONE);
 		
-		println("Integer part: " + integerPart);
-		println("Franction part (as BigDecimal): " + franctionPart);
 		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+////		task 17
+//		calculateNumInBigDecimal();
+//		
+//	}
+//
+//	private void calculateNumInBigDecimal() {
+//		double a = readDouble ("enter real number: " );
+//		String a_AsString = "" + a;
+//		BigDecimal decimal = new BigDecimal (a_AsString);
+//		
+//		int integerPart = decimal.intValue() ;
+//		BigDecimal franctionPart = decimal.remainder(BigDecimal.ONE);
+//		
+//		println("Integer part: " + integerPart);
+//		println("Franction part (as BigDecimal): " + franctionPart);
+//		
 	}
 }
