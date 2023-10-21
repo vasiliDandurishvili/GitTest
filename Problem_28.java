22b394c7b2e58fbee5eeadd630ad70f10d682698
S <s>
2023-10-21 17:31:49 +0400

    {"types":"C","runs":50}

--- Problem_28.java
+++ Problem_28.java
@@ -1,4 +1,22 @@
 
-public class Problem_28 {
-
+import acm.program.ConsoleProgram;
+/*
+მომხმარებელს შეყავს დადებითი რიცხვები, მანამ სანამ არ შეიყვანს -1 ს, დაბეჭდეთ
+რაოდენობა რამდენი ლუწი რიცხვი შეყვანა მომხმარებელმა.
+ */
+public class Problem_28 extends ConsoleProgram {
+	public void run () {
+		int numOfEvenIntegers = 0;
+		while(true){
+			int num_1 = readInt("enter the true number: ");
+			boolean a = num_1 % 2 == 0;
+			if(a){
+				numOfEvenIntegers += 1;
+			}
+			if(num_1 == -1){
+				break;
+			}
+		}
+		println("Number of even integers: " + numOfEvenIntegers);
+	}
 }
