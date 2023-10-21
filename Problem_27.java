5cd29f74dfbc760d11283a834701f08e32d6c06e
S <s>
2023-10-22 03:00:28 +0400

    {"types":"C","runs":0}

--- Problem_27.java
+++ Problem_27.java
@@ -6,9 +6,12 @@ public class Problem_27 extends ConsoleProgram {
 	public void run () {
 		int n = readInt("enter the integer: ");
 		int m = readInt("enter the integer: ");
+		
+		int n_MultipleXm = 1;
+		
 		for (int i = 0; i < m; i++){
-// 
-//			n =
+			n_MultipleXm *= n;
+
 		}
 		println("n^m: " + n);
 	}
