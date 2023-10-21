1b8a2eb7e009ac0aa1b1e3d3f5b3d221e2eed39c
S <s>
2023-10-21 16:53:54 +0400

    {"types":"C","runs":44}

--- Problem_25.java
+++ Problem_25.java
@@ -13,7 +13,7 @@ public class Problem_25 extends ConsoleProgram {
 		for (int i = 1; i <= num_1;i++){
 			boolean divisionNum_1_WithoutRemainder = num_1 % i == 0;
 			if(divisionNum_1_WithoutRemainder){
-				for(int j = 1; j <= num_2; i++){
+				for(int j = 1; j <= num_2; j++){
 					boolean divisionNum_2_WithoutRemainder = num_2 % j == 0;
 					if(divisionNum_2_WithoutRemainder){
 						if(i == j){
