ae7bc9cd27c454d265fd39f96e43bb69af9ee759
S <s>
2023-10-21 13:59:28 +0400

    {"types":"C","runs":15}

--- Problem_24.java
+++ Problem_24.java
@@ -1,4 +1,19 @@
+import acm.program.ConsoleProgram;
+/*
+ მომხმარებელს შეყავს მთელი რიცხვი n, პროგრამამ უნდა დაბეჭდოს n ის გამყოფების
+რაოდენობა
+ */
+public class Problem_24 extends ConsoleProgram {
+	public void run () {
+		int num_1 = readInt("enter the integer: ");
+		int numOfDividers = 0;
+		for (int i = 1; i <= num_1; i++){
+			boolean a = num_1 % i == 0;
+			if (a){
+				numOfDividers +=1;
+			}
+			
+		}
+	}
+}	
 
-public class Problem_24 {
-
-}
