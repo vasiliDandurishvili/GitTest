e115d27c34cadb249b86e3cf2c199861b3d29f92
S <s>
2023-10-24 17:03:09 +0400

    {"types":"C","runs":2}

--- Problem_29.java
+++ Problem_29.java
@@ -1,4 +1,12 @@
-
-public class Problem_29 {
-
+import acm.program.ConsoleProgram;
+/*
+მოხმარებელს შეყავს მთელი რიცხვი n, პროგრამამ უნდა დაბეჭდოს ფიბონაჩის მიმდევრობის
+მე n წევრი.
+ */
+public class Problem_29 extends ConsoleProgram {
+	public void run () {
+		int n = readInt ("enter the integer: ");
+		
+		
+	}
 }
