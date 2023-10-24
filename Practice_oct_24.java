57980bb413acd0b6a76bdcf50a521c16e2076f38
S <s>
2023-10-24 17:34:36 +0400

    {"types":"C","runs":6}

--- Practice_oct_24.java
+++ Practice_oct_24.java
@@ -4,37 +4,63 @@ import acm.program.ConsoleProgram;
 ჯამი
 24. მომხმარებელს შეყავს მთელი რიცხვი n, პროგრამამ უნდა დაბეჭდოს n ის გამყოფების
 რაოდენობა
+25. მომხმარებელს შეჰყავს ორი რიცხვი თქვენმა პროგრამამ უნდა დაბეჭდოს ამ ორი რიცხვის
+უმცირესი საერთო ჯერადი.
+26. მომხმარებელს შეჰყავს ორი რიცხვი თქვენმა პროგრამამ უნდა დაბეჭდოს ამ ორი რიცხვის
+უდიდესი საერთო გამყოფი
  */
 public class Practice_oct_24 extends ConsoleProgram{
 	public void run () {
-	
-	int n = readInt("enter n: ");
-	int sum = calculateSum(n);
-	println("sum of numbers 1...n is: " + sum);
-	
-	
-	int numDivisors = numberOfDividers( n);
-	println("num of divisors: " + numDivisors);
+		int n = readInt("enter n: ");
+		int m = readInt("enter m: ");
+		
+		int gcd = gcd(n,m);
+		println("gcd is: " + gcd)
+		
+		
+		
 	}
 
-	private int calculateSum(int n) {
-		int sum = 0;
-		for(int i = 1; i <= n; i++){
-			sum += i;
-		}
-		return sum;
+	private int gcd(int n, int m) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
-	
-	private int numberOfDividers(int n){
-		int num = 0;
-		for (int i = 1; i <= n; i++){
-			if(n % i == 0){
-				num += 1;
-			}
-		}
-		return num;
-	}
-	
 }
 
+		
+		
+		
+		
+		
+		
+		
+//		23 da 24
+//	int n = readInt("enter n: ");
+//	int sum = calculateSum(n);
+//	println("sum of numbers 1...n is: " + sum);
+//	
+//	
+//	int numDivisors = numberOfDividers( n);
+//	println("num of divisors: " + numDivisors);
+//	}
+//
+//	private int calculateSum(int n) {
+//		int sum = 0;
+//		for(int i = 1; i <= n; i++){
+//			sum += i;
+//		}
+//		return sum;
+//	}
+//	
+//	private int numberOfDividers(int n){
+//		int num = 0;
+//		for (int i = 1; i <= n; i++){
+//			if(n % i == 0){
+//				num += 1;
+//			}
+//		}
+//		return num;
+//	}
+//	
+	
 
