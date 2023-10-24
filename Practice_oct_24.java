6b398ef6cbd54ff8109c2b1cd4e81304ed947915
S <s>
2023-10-24 17:17:49 +0400

    {"types":"C","runs":2}

--- Practice_oct_24.java
+++ Practice_oct_24.java
@@ -15,9 +15,11 @@ public class Practice_oct_24 extends ConsoleProgram{
 	}
 
 	private int calculateSum(int n) {
-		// TODO Auto-generated method stub
-		return 0;
+		int sum = 0;
+		for(int i = 1; i <= n; i++){
+			sum += i;
+		}
+		return sum;
 	}
 	
-
 }
