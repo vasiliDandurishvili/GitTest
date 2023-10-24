a6742f5b2dc98891c51c7d7a8d354c2c5632355e
S <s>
2023-10-24 17:10:07 +0400

    {"types":"C","runs":2}

--- Practice_oct_24.java
+++ Practice_oct_24.java
@@ -1,4 +1,23 @@
+import acm.program.ConsoleProgram;
+/*
+ 23. მომხმარებელს შეყავს მთელი რიცხვი n, პროგრამამ უნდა დაბეჭდოს 1 დან n მდე რიცხვების
+ჯამი
+24. მომხმარებელს შეყავს მთელი რიცხვი n, პროგრამამ უნდა დაბეჭდოს n ის გამყოფების
+რაოდენობა
+ */
+public class Practice_oct_24 extends ConsoleProgram{
+	public void run () {
+	
+	int n = readInt("enter n: ");
+	int sum = calculateSum(n);
+	println("sum of numbers 1...n is: " + sum);
+	
+	}
 
-public class Practice_oct_24 {
+	private int calculateSum(int n) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
 
 }
