fe86c6276df83472c1609d93be939e83b51302d2
S <s>
2023-10-26 22:03:13 +0400

    {"types":"C","runs":0}

--- Problem_30.java
+++ Problem_30.java
@@ -1,4 +1,12 @@
-
-public class Problem_30 {
-
+import acm.program.ConsoleProgram;
+/*
+მომხმარებელს შეყავს მთელი რიცხვი, დაბეჭდეთ რიცხვი რომელიც მიიღება ციფრების
+შებრუნებით, 1256 -> 6521
+ */
+public class Problem_30 extends ConsoleProgram {
+	public void run () {
+		int n = readInt("enter the integer: ");
+//		string m = string reverse(n);
+		String m = "isis";
+	}
 }
