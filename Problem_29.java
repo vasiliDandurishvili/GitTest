c3d644950ad4b5531abdf84edaaebdc79d048731
S <s>
2023-10-26 17:55:13 +0400

    {"types":"C","runs":2}

--- Problem_29.java
+++ Problem_29.java
@@ -8,8 +8,22 @@ public class Problem_29 extends ConsoleProgram {
 		int n = readInt ("enter the integer: ");
 		int firstNum = 0;
 		int secondNum = 1;
-		println("findFibonachNum: " + findFibonachiNum(secondNum, n));
+		int k = 0;
+		println("findFibonachNum: " + findFibonachiNum(firstNum, secondNum, n, k));
+	}
 
-		
+	private int findFibonachiNum(int firstNum, int secondNum, int n, int k) {
+		if(n == 1){
+			return firstNum;
+		}
+		if(n == 2){
+			return secondNum;
+		}
+		for(int i = 2; i <= n; i++){
+			k = firstNum + secondNum;
+			firstNum = secondNum;
+			secondNum = k;
+		}
+		return -1;
 	}
 }
