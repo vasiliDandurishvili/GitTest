654f1cb5f5866201dfdd316067f312b83344298e
S <s>
2023-10-26 22:18:40 +0400

    {"types":"C","runs":0}

--- Practice_Class_2.java
+++ Practice_Class_2.java
@@ -1,4 +1,12 @@
 
-public class Practice_Class_2 {
+import acm.graphics.GLabel;
+import acm.program.*;
+
+public class Practice_Class_2  extends GraphicsProgram {
+	
+	public void run() {
+	GLabel msg = new GLabel("hello worls", 20, 20);	
+	add(GLabel);
+	}
 
 }
