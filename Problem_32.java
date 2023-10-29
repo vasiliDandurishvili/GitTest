114c41f681d10033775f0a004d5e2f2b0900f03f
S <s>
2023-10-29 21:01:31 +0400

    {"types":"C","runs":98}

--- Problem_32.java
+++ Problem_32.java
@@ -1,4 +1,27 @@
+import acm.program.ConsoleProgram;
 
-public class Problem_32 {
+/** 32. დაწერეთ პროგრამა რომელიც იპოვის და დაბეჭდავს 1000000 მდე ყველა მერტივ რიცხვს */
+
+
+public class Problem_32 extends ConsoleProgram {
+	public void run(){
+		findThePrimes();
+		
+	}
+
+	private void findThePrimes() {
+		for(int i = 1; i <= 1000000; i++){
+			int numOfDividers = 0;
+			for(int j = 1; j <= 1000000; i++){
+				if(i % j == 0 ){
+					numOfDividers += 1;
+				}
+			}
+			if(numOfDividers == 2){
+				println("the prime integer: " + i);
+			}
+		}
+		
+	}
 
 }
