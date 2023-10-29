8b69646bb3966ad572ffa15d11628f696da79dd0
S <s>
2023-10-29 20:38:35 +0400

    {"types":"C","runs":92}

--- Problem_30.java
+++ Problem_30.java
@@ -8,14 +8,14 @@ import acm.program.ConsoleProgram;
 public class Problem_30 extends ConsoleProgram {
 	public void run () {
 		int n = readInt("enter the integer: ");
-		for(int i = 0; i < 3;i++ ){
-			int divide = n / 10;
-			double fraction = n / 10;
-			int reverseNum = (int) (fraction * 10 - divide * 10);
-			print(reverseNum);
-			if(divide == 0){
-				break;
-			}
+		int reverseNum = 0;
+		
+		while(n != 0){
+			int lastInt = n % 10;
+			reverseNum = lastInt * 10 + lastInt;
+			
+			n /= n;
 		}
+		println("reversed num: " + reverseNum);
 	}
 }
