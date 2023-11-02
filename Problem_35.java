fe73b3b72526b6007f27a66f2477fa01bbd40cf4
S <s>
2023-11-02 16:40:09 +0400

    {"types":"C","runs":0}

--- Problem_35.java
+++ Problem_35.java
@@ -1,4 +1,35 @@
+//35. თქვენი ამოცანაა გააკეთოთ მონეტის აგდების სიმულაციები და დათვალოთ საშუალოდ
+//რამდენჯერ უნდა ავაგდოთ მონეტა რათა ამოვიდეს ბორჯღალო
 
-public class Problem_35 {
+import acm.program.ConsoleProgram;
+import acm.util.RandomGenerator;
 
+public class Problem_35 extends ConsoleProgram{
+	private static final int MAX_SIMULATIONS  = 10000;
+	private RandomGenerator rgen  = RandomGenerator.getInstance();
+	public void run() {
+		print(rgen);
+		double sumFlips = 0;
+		for(int i = 0; i < MAX_SIMULATIONS; i++) {
+			int currNumFlips = simulation();
+			sumFlips += currNumFlips;
+		}
+
+		double avgFlips = sumFlips / MAX_SIMULATIONS;
+
+		println("average number of flips: " + avgFlips);
+	}
+
+	private int simulation() {
+		int numFlips = 0;
+		while(true) {
+			boolean isHeads = rgen.nextBoolean();
+			numFlips += 1;
+			if(isHeads) {
+				break;
+			}
+		}
+		return numFlips;
+	}
 }
+
