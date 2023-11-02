90129d395e17daba984ffc5160680b2281675809
S <s>
2023-11-02 17:57:34 +0400

    {"types":"C","runs":6}

--- Problem_36.java
+++ Problem_36.java
@@ -1,4 +1,44 @@
+import acm.program.ConsoleProgram;
+import acm.util.RandomGenerator;
 
-public class Problem_36 {
+//36. თქვენი ამოცანაა გააკეთოთ მონეტის აგდების სიმულაციები და დათვალოთ საშუალოდ
+//რამდენჯერ უნდა ავაგდოთ მონეტა რათა 3-ჯერ ზედიზედ ამოვიდეს ბორჯღალო.
 
+
+public class Problem_36 extends ConsoleProgram{
+	
+	private static final int MAX_EXPERIMENTS  = 100000;
+	private RandomGenerator rgen  = RandomGenerator.getInstance();
+	
+	public void run() {
+		double sumFlips = 0;
+		for( int i = 0; i < MAX_EXPERIMENTS; i++ ){
+			int currFlips = holdExperiment();
+			sumFlips += currFlips;
+		}
+		double avgFlips = sumFlips / MAX_EXPERIMENTS;
+		println(avgFlips);
+	}
+	
+	
+
+	private int holdExperiment() {
+		int numFlips = 0;
+		int numConsecutiveHeads = 0;
+		while(true){
+			numFlips++;
+			boolean isHeads = rgen.nextBoolean();
+			if(isHeads){
+				numConsecutiveHeads += 1;
+				if(numConsecutiveHeads == 3){
+					break;
+				}
+			}else{
+				numConsecutiveHeads = 0;
+				
+			}
+		}
+	return numFlips;
+	}
 }
+
