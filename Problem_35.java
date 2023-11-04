55f7530c2e515d998799f66a86a989dc7cc5aa45
S <s>
2023-11-05 00:00:39 +0400

    {"types":"C","runs":50}

--- Problem_35.java
+++ Problem_35.java
@@ -6,7 +6,7 @@ import acm.util.RandomGenerator;
 
 public class Problem_35 extends ConsoleProgram{
 	
-	private static final int MAX_EXPERIMENTS  = 1;
+	private static final int MAX_EXPERIMENTS  = 10;
 	private RandomGenerator rgen  = RandomGenerator.getInstance();
 	
 	public void run() {
