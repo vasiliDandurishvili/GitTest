8d345244f372afe7fb229a80cdb23b2c4bec8998
S <s>
2023-11-07 17:53:27 +0400

    {"types":"C","runs":16}

--- Problem_39.java
+++ Problem_39.java
@@ -5,12 +5,23 @@ import acm.program.GraphicsProgram;
 //წერტილიდან მარჯვენა კედლამდე.
 public class Problem_39 extends GraphicsProgram{
 	private static final int R = 100;
+	private static final int PAUSE_TIME = 1000;
+
 	public void run(){
 		GOval myOval = new GOval (R,R);
 		double x = 0;
 		double y = getHeight() / 2 - R / 2;
 		
 		add(myOval,x,y);
+		
+		getCircleToTheCentre( x, myOval);
+	}
+	private void getCircleToTheCentre(double x,GOval myOval) {
+		while (x < getWidth() / 2 ){
+			myOval.move(1,0);
+			pause(PAUSE_TIME);
+		}
+		
 	}
 
 }
