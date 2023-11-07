10ab02c89f2cd8276437aef42a973c5af3e5b26b
S <s>
2023-11-07 17:53:42 +0400

    {"types":"C","runs":17}

--- Problem_39.java
+++ Problem_39.java
@@ -18,7 +18,7 @@ public class Problem_39 extends GraphicsProgram{
 	}
 	private void getCircleToTheCentre(double x,GOval myOval) {
 		while (x < getWidth() / 2 ){
-			myOval.move(1,0);
+			myOval.move(5,0);
 			pause(PAUSE_TIME);
 		}
 		
