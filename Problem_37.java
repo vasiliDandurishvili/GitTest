8763d2f77f154c02564078dd8a1cd16ba50b5ed0
S <s>
2023-11-07 17:15:17 +0400

    {"types":"C","runs":7}

--- Problem_37.java
+++ Problem_37.java
@@ -1,37 +1,40 @@
 import acm.program.*;
 import acm.util.RandomGenerator;
 
-public class Problem_37 extends ConsoleProgram{
+public class Problem_37 extends ConsoleProgram {
 	private static final int MIN_VALUE = 0;
 	private static final int MAX_VALUE = 36;
 	private RandomGenerator rgen = RandomGenerator.getInstance();
-	public void run(){
+
+	public void run() {
 		int betNumber = readBet();
 		int rouletteNumber = spinRoulette();
-		
-		if(betNumber == rouletteNumber){
+
+		if (betNumber == rouletteNumber) {
 			println("you won!");
-		}else{
+		} else {
 			println("you lose!");
 		}
 	}
 
 	private int spinRoulette() {
-		int rouletteReslt = rgen.nextInt(MIN_VALUE,MAX_VALUE);
+		int rouletteReslt = rgen.nextInt(MIN_VALUE, MAX_VALUE);
 		println("roulette result is: " + rouletteReslt);
-		
+
 		return rouletteReslt;
 	}
 
 	private int readBet() {
 		int bet = readInt("enter bet num(0-36): ");
-		if(bet < 0 || bet > 36){
-			bet = readInt("enter bet num(0-36): ");
-		}else{
+		while (true) {
+			if (bet < 0 || bet > 36) {
+				bet = readInt("enter bet num(0-36): ");
+			} else {
+				return bet;
+			}
+
 			return bet;
 		}
-		
-		return bet;
-	}
 
+	}
 }
