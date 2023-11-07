434b9ac7eb9aea95e6a75d2dbefda3fb010f0706
S <s>
2023-11-07 17:47:09 +0400

    {"types":"C","runs":14}

--- Problem_39.java
+++ Problem_39.java
@@ -4,7 +4,7 @@ import acm.program.GraphicsProgram;
 //39. დაწერეთ პროგრამა სადაც იქნება კანვასზე ბურთის მოძრაობა მარცხენა კედლის შუა
 //წერტილიდან მარჯვენა კედლამდე.
 public class Problem_39 extends GraphicsProgram{
-	private static final int R = 30;
+	private static final int R = 100;
 	public void run(){
 		GOval myOval = new GOval (R,R);
 		double x = 0;
