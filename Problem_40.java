c019073a7790fc8cf1cc24a3fd88cfeaf43f2c8d
S <s>
2023-11-11 16:29:06 +0400

    {"types":"C","runs":42}

--- Problem_40.java
+++ Problem_40.java
@@ -1,4 +1,25 @@
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
 
-public class Problem_40 {
+//40. წინა ამოცანის გაგრძელება, დაამატეთ ვერტიკალური კედლებიდან ასხლეტვა. ანუ ბურთი
+//მოძრაობს მარცხენა კედლიდან მარჯვნივ. როგორც კი მარჯვენა კედელს დაეტაკება იმავე
+//სიჩქარით უნდა გააგრძელოს მოძრაობა მაჯვენა კედლიდან მარცხნივ. როგორც კი მარცხენა
+//კედელს დაეტაკება უნდა გააგრძელოს მარჯვნივ და ა.შ. უსასრულოდ.
 
+public class Problem_40 extends GraphicsProgram {
+	private static final int PAUSE = 5;
+	
+	private static final int RADIUS = 20;
+	public void run(){
+		GOval circle = createLeftMidlleCircle();
+	}
+	private GOval createLeftMidlleCircle() {
+		GOval circle = new GOval (2 * RADIUS, 2 * RADIUS);
+		circle.setFilled(true);
+		double x = 0;
+		double y = getHeight() - RADIUS;
+		add(circle, x, y);
+		return circle;
+	}
+	
 }
