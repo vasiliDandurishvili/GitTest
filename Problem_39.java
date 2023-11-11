b17a07c81c2a98277b5fa8e6b3daa8776b12a8f7
S <s>
2023-11-11 15:06:42 +0400

    {"types":"C","runs":25}

--- Problem_39.java
+++ Problem_39.java
@@ -4,28 +4,28 @@ import acm.program.GraphicsProgram;
 //39. დაწერეთ პროგრამა სადაც იქნება კანვასზე ბურთის მოძრაობა მარცხენა კედლის შუა
 //წერტილიდან მარჯვენა კედლამდე.
 public class Problem_39 extends GraphicsProgram {
-	private static final int RADIUS = 100;
+	private static final int R = 100;
 	private static final int PAUSE_TIME = 100;
 
 	public void run() {
-//		GOval myOval = new GOval(R * 2, R * 2);
-//		double x = 0;
-//		double y = getHeight() / 2 - R ;
-//
-//		add(myOval, x, y);
-//
-//		getCircleToTheCentre(x, myOval);
+		GOval myOval = new GOval(R * 2, R * 2);
+		double x = 0;
+		double y = getHeight() / 2 - R ;
+
+		add(myOval, x, y);
+
+		getCircleToTheCentre(x, myOval);
 		
-		GOval circle = createInitialCircle();
+
 		
 		
 	}
 
-//	private void getCircleToTheCentre(double x, GOval myOval) {
-//		while (myOval.getX() < getWidth() / 2) {
-//			myOval.move(40, 0);
-//			pause(PAUSE_TIME);
-//		}
+	private void getCircleToTheCentre(double x, GOval myOval) {
+		while (myOval.getX() < getWidth() / 2) {
+			myOval.move(40, 0);
+			pause(PAUSE_TIME);
+		}
 
 	}
 
