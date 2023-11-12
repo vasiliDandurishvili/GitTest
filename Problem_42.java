2b2c057820a976e95fd9060fc58671905d6c119d
S <s>
2023-11-12 12:54:12 +0400

    {"types":"C","runs":1}

--- Problem_42.java
+++ Problem_42.java
@@ -1,3 +1,4 @@
+
 /*
  * File: Breakout.java
  * -------------------
@@ -17,76 +18,66 @@ import java.awt.event.*;
 
 public class Problem_42 extends GraphicsProgram {
 
-/** Width and height of application window in pixels */
+	/** Width and height of application window in pixels */
 	public static final int APPLICATION_WIDTH = 400;
 	public static final int APPLICATION_HEIGHT = 600;
 
-/** Dimensions of game board (usually the same) */
+	/** Dimensions of game board (usually the same) */
 	private static final int WIDTH = APPLICATION_WIDTH;
 	private static final int HEIGHT = APPLICATION_HEIGHT;
 
-/** Dimensions of the paddle */
+	/** Dimensions of the paddle */
 	private static final int PADDLE_WIDTH = 60;
 	private static final int PADDLE_HEIGHT = 10;
 
-/** Offset of the paddle up from the bottom */
+	/** Offset of the paddle up from the bottom */
 	private static final int PADDLE_Y_OFFSET = 30;
 
-/** Number of bricks per row */
+	/** Number of bricks per row */
 	private static final int NBRICKS_PER_ROW = 10;
 
-/** Number of rows of bricks */
+	/** Number of rows of bricks */
 	private static final int NBRICK_ROWS = 10;
 
-/** Separation between bricks */
+	/** Separation between bricks */
 	private static final int BRICK_SEP = 4;
 
-/** Width of a brick */
-	private static final int BRICK_WIDTH =
-	  (WIDTH - (NBRICKS_PER_ROW - 1) * BRICK_SEP) / NBRICKS_PER_ROW;
+	/** Width of a brick */
+	private static final int BRICK_WIDTH = (WIDTH - (NBRICKS_PER_ROW - 1) * BRICK_SEP) / NBRICKS_PER_ROW;
 
-/** Height of a brick */
+	/** Height of a brick */
 	private static final int BRICK_HEIGHT = 8;
 
-/** Radius of the ball in pixels */
+	/** Radius of the ball in pixels */
 	private static final int BALL_RADIUS = 10;
 
-/** Offset of the top brick row from the top */
+	/** Offset of the top brick row from the top */
 	private static final int BRICK_Y_OFFSET = 70;
 
-/** Number of turns */
+	/** Number of turns */
 	private static final int NTURNS = 3;
 
-/* Method: run() */
-/** Runs the Breakout program. */
+	/* Method: run() */
+	/** Runs the Breakout program. */
 	public void run() {
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		/** draw bricks in 10 row also 10 in each and paint in
+		 * appropriate color.
+		 *    */
+		for (int i = 0; i < 10; i++) {
+			fillEachLineWithBricks(i);
+		}
 
 	}
+
+	private void fillEachLineWithBricks(int i) {
+	for(int j = 0; j < 10; j++){
+		GRect brick = new GRect (BRICK_WIDTH, BRICK_HEIGHT);
+		double x = WIDTH - 10 * (BRICK_WIDTH + BRICK_SEP);
+		double y = BRICK_Y_OFFSET;
+		
+		add(brick, x, y);
+	}
+	
+}
 
 }
