4f99985b7ba119726b6a7f5c028f8039d84034d4
S <s>
2023-11-13 10:48:12 +0400

    {"types":"C","runs":13}

--- jsjs.java
+++ jsjs.java
@@ -1,4 +1,14 @@
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
 
-public class jsjs {
+public class jsjs extends GraphicsProgram{
+	GRect r;
+	public void run(){
+		r = new GRect (20,20,20,20);
+		add(r);
+		
+		
+		getElementAt(30,30);
+	}
 
 }
