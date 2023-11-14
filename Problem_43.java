6255616a2df211e0638cf6b84e27926e34002034
S <s>
2023-11-14 17:40:43 +0400

    {"types":"C","runs":36}

--- Problem_43.java
+++ Problem_43.java
@@ -1,4 +1,15 @@
-
-public class Problem_43 {
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
+//43. წრეწირის drag ით გადაადგილება
+public class Problem_43 extends GraphicsProgram {
+	private static final int RADIUS = 50;
+	private GOval circle;
+	public void run(){
+		circle = new GOval(2 * RADIUS, 2 * RADIUS );
+		circle.setFilled(true);
+		add(circle, getWidth() / - RADIUS,  getHeight() / - RADIUS );
+		addMouseListeners();
+		
+	}
 
 }
