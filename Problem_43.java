d335d6ea8de34b678138e60236fcafe4255dda0c
S <s>
2023-11-14 17:47:57 +0400

    {"types":"C","runs":39}

--- Problem_43.java
+++ Problem_43.java
@@ -1,42 +1,41 @@
 
-
-
-
 import java.awt.event.MouseEvent;
 
 import acm.graphics.*;
 import acm.program.GraphicsProgram;
+
 //43. წრეწირის drag ით გადაადგილება
 public class Problem_43 extends GraphicsProgram {
 	private static final int RADIUS = 50;
 	private GOval circle;
 	private int prevX;
-	private int prevY;	
-	
-	public void run(){
-		circle = new GOval(2 * RADIUS, 2 * RADIUS );
+	private int prevY;
+
+	public void run() {
+		circle = new GOval(2 * RADIUS, 2 * RADIUS);
 		circle.setFilled(true);
-		add(circle, getWidth() / 2 - RADIUS,  getHeight() / 2 - RADIUS );
+		add(circle, getWidth() / 2 - RADIUS, getHeight() / 2 - RADIUS);
 		addMouseListeners();
-		
+
 	}
 
-	public void mousePressed(MouseEvent e){
+	public void mousePressed(MouseEvent e) {
 		GObject obj = getElementAt(e.getX(), e.getY());
-		if (obj != null){
+		if (obj != null) {
 			prevX = e.getX();
 			prevY = e.getY();
 		}
-		}
-		public void mouseDragged(MouseEvent e){
-			GObject obj = getElementAt(e.getX(), e.getY());
-			if (obj != null){
-				
+	}
+
+	public void mouseDragged(MouseEvent e) {
+		GObject obj = getElementAt(e.getX(), e.getY());
+		if (obj != null) {
+
 			// move object
-				obj.move(e.getX() - prevX, e.getY() - prevY);
-			
-				prevX = e.getX();
-				prevY = e.getY();
-			}
+			obj.move(e.getX() - prevX, e.getY() - prevY);
+
+			prevX = e.getX();
+			prevY = e.getY();
+		}
 	}
 }
