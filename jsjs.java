5edf997672698b08b7ac1485f752e2fb86d42a71
S <s>
2023-11-14 10:51:13 +0400

    {"types":"C","runs":14}

--- jsjs.java
+++ jsjs.java
@@ -2,11 +2,15 @@ import acm.graphics.*;
 import acm.program.GraphicsProgram;
 
 public class jsjs extends GraphicsProgram{
+	private static final int PAUSE= 10;
 	GRect r;
 	public void run(){
 		r = new GRect (20,20,20,20);
 		add(r);
-		
+		while(r.getX() - 10 < getWidth() / 2){
+		r.move(5, 0);
+		pause(PAUSE);
+		}
 		
 		GObject x = getElementAt(30,30);
 		if (x != null){
