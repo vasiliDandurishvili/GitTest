e5aab7ed008408b3984b605fbc408b257ae1f076
S <s>
2023-11-14 17:47:15 +0400

    {"types":"C","runs":38}

--- Problem_43.java
+++ Problem_43.java
@@ -1,9 +1,18 @@
+
+
+
+
+import java.awt.event.MouseEvent;
+
 import acm.graphics.*;
 import acm.program.GraphicsProgram;
 //43. წრეწირის drag ით გადაადგილება
 public class Problem_43 extends GraphicsProgram {
 	private static final int RADIUS = 50;
 	private GOval circle;
+	private int prevX;
+	private int prevY;	
+	
 	public void run(){
 		circle = new GOval(2 * RADIUS, 2 * RADIUS );
 		circle.setFilled(true);
@@ -12,4 +21,21 @@ public class Problem_43 extends GraphicsProgram {
 		
 	}
 
+	public void mousePressed(MouseEvent e){
+		GObject obj = getElementAt(e.getX(), e.getY());
+		if (obj != null){
+			prevX = e.getX();
+			prevY = e.getY();
+		}
+		public void mouseDragged(MouseEvent e){
+			GObject obj = getElementAt(e.getX(), e.getY());
+			if (obj != null){
+				
+			// move object
+				obj.move(e.getX() - prevX, e.getY() - prevY);
+			
+				prevX = e.getX();
+				prevY = e.getY();
+			}
+	}
 }
