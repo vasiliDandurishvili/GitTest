e85e2485ccb0d96c88986eef3ffaa210be486921
S <s>
2023-11-14 17:59:18 +0400

    {"types":"C","runs":41}

--- Problem_44.java
+++ Problem_44.java
@@ -1,4 +1,21 @@
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
+import java.awt.event.MouseEvent;
+//44. მაუსის კლიკზე იმავე ადგილზე ჩნდება ბურთი და იწყებს ვარდნას ძირს, თუ მაუსს სხვა
+//ადგილზე ვაკლიკებ ბურთი გადადის ახალ ადგილზე და იქედან იწყებს ვარდნას
+public class Problem_44 extends GraphicsProgram {
+	private static int RADIUS = 50;
+	GOval circle;
+	
+	public void run(){
+		circle = new GOval ( 2 * RADIUS, 2* RADIUS );
+		circle.setFilled(true);
+		addMouseListeners();
+	}
 
-public class Problem_44 {
-
+	@Override
+	public void mouseClicked(MouseEvent e){
+		add(circle, e.getX() - RADIUS, e.getY() - RADIUS);
+	}
+	
 }
