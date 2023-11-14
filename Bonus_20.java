a5a06e6ab7f3c69abf5234d881900ab316267264
S <s>
2023-11-14 16:42:38 +0400

    {"types":"C","runs":16}

--- Bonus_20.java
+++ Bonus_20.java
@@ -1,4 +1,27 @@
+import acm.program.ConsoleProgram;
+import acm.util.RandomGenerator;
 
-public class Bonus_20 {
+public class Bonus_20 extends ConsoleProgram{
+	RandomGenerator g = RandomGenerator.getInstance();
+	public void run(){
+		int a = 0;
+		double c = 0;
+		for(int i = 0; i< 1000000; i++){
+			
+			for(int t = 0; t< 323; t++){
+				int m = g.nextInt(-1,100) ;
+				a += m;
+				
+				
+			}
+			c += a/322.0;
+			a =0;
+			
+		}
+		
+		println(c/1000000);
+		
+		
+	}
 
 }
