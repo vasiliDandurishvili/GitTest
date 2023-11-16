682cf25bb9b2bb9d13c56518a6e3029b7bc2f89f
S <s>
2023-11-16 17:33:52 +0400

    {"types":"C","runs":99}

--- Problem_45.java
+++ Problem_45.java
@@ -3,20 +3,18 @@ import acm.program.ConsoleProgram;
 //45. მომხმარებელს კონსოლიდან შეჰყავს ტექსტი და სიმბოლო, თქვენმა პროგრამამ უნდა იპოვოს
 //რამდენჯერ გვხვდება მოცემულ ტექსტში მოცემული სიმბოლო
 public class Problem_45 extends ConsoleProgram {
-	public void run(){
+	public void run() {
 		int x = 0;
-	String text = readLine("enter text: ");
-	String symbolText = readLine("enter symbol: ");
-	
-	char symbol = symbolText.charAt(0);
-	
-	for(int i = 0; i < text.length(); i++){
-	if(symbol == text.charAt(i)){
-		x++;
-	}
+		String text = readLine("enter text: ");
+		String symbolText = readLine("enter symbol: ");
 
-		
-	}
-	print(x);
+		char symbol = symbolText.charAt(0);
+
+		for (int i = 0; i < text.length(); i++) {
+			if (symbol == text.charAt(i)) {
+				x++;
+			}
+		}
+		print(x);
 	}
 }
