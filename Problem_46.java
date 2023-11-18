4530637f2cc7cb137a4fdf4c777a3316c36dcfd5
S <s>
2023-11-18 09:07:30 +0400

    {"types":"C","runs":101}

--- Problem_46.java
+++ Problem_46.java
@@ -1,4 +1,14 @@
+import acm.program.ConsoleProgram;
 
-public class Problem_46 {
-
+//46. დაწერეთ მეთოდი რომელსაც გადაეცემა სტიგნი და რომელიც აბრუნებს ამ სტრინგში
+//ყველაზე ხშირ სიმბოლოს.
+public class Problem_46 extends ConsoleProgram {
+	public void run(){
+		String text = readLine("enter string: ");
+		
+	}
+	
+	private void numOccure(String tect, char symbol){
+		
+	}
 }
