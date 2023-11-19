487f6f84deef9ade699c5a7ba4e160845ffda595
S <s>
2023-11-19 13:45:53 +0400

    {"types":"C","runs":283}

--- jsjs.java
+++ jsjs.java
@@ -2,13 +2,15 @@ import acm.graphics.*;
 import acm.program.GraphicsProgram;
 
 public class jsjs extends GraphicsProgram{
+	private static final int vx = 3;
     GOval oval;
 	public void run(){
 		oval = new GOval (30,30);
 		add(oval,0,getHeight() / 2 - 15);
 		
-		while(oval.getX() + 30 < getWidth()){
-		oval.move(2, 0);
+		while(oval.getX() + 30 < getWidth() || oval.getX() < 0){
+			vx = -vx;
+		oval.move(vx, 0);
 		pause(10);
 		}
 	}
