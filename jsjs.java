2795e427d23328948717170cc3fda2d75e0990a8
S <s>
2023-11-19 20:09:31 +0400

    {"types":"C","runs":0}

--- jsjs.java
+++ jsjs.java
@@ -7,8 +7,14 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class jsjs extends GraphicsProgram {
+	GLine line;
 	public void run(){
-		
-		
+		addMouseListeners();
 	}
+	public void mousePressed(MouseEvent e){
+		line = new GLine(e.getX(),e.getY(),e.getX(),e.getY() );
+		add(line);
+	}
+	
+	
 }
