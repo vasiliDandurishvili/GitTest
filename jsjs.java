15399e4dbe3c8b81d315496de9223b28f8b69263
S <s>
2023-11-19 19:57:11 +0400

    {"types":"AR","runs":0}

---
+++ jsjs.java
@@ -0,0 +1,14 @@
+import acm.graphics.*;
+import acm.program.*;
+import acm.util.*;
+
+import java.applet.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class jsjs extends GraphicsProgram {
+	public void run(){
+		
+		
+	}
+}
