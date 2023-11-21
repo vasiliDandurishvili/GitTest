5ed2bff8cc8fc02c166640cd1a4b85f94ae9cf42
S <s>
2023-11-21 17:44:05 +0400

    {"types":"C","runs":15}

--- Problem_48.java
+++ Problem_48.java
@@ -1,4 +1,15 @@
+import acm.program.ConsoleProgram;
 
-public class Problem_48 {
+//48. კონსოლიდან შეგყვავს ტექსტი პროგრამამ უნდა დაბეჭდოს სტატისტიკა რომელი სიმბოლო
+//რამდენჯერ გვხვდება
+public class Problem_48 extends ConsoleProgram {
+	public void run() {
+		String text = "mariami";
+		printCharStatistics(text);
+	}
 
+	private void printCharStatistics(String text) {
+		
+		
+	}
 }
