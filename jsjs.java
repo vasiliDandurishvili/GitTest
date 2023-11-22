58176e63a72bfbdb4943f4200eca2fff5d7041a7
S <s>
2023-11-22 23:28:37 +0400

    {"types":"C","runs":34}

--- jsjs.java
+++ jsjs.java
@@ -5,51 +5,26 @@ import acm.util.*;
 import java.applet.*;
 import java.awt.*;
 import java.awt.event.*;
-// Roman to Integer.
-public class jsjs extends ConsoleProgram {
-
-	public void run() {
-		int ans = 0;
-		String text = readLine("enter Roman numbers: ");
-		
-		for(int i = 0; i < text.length(); i++){
-			if(text.charAt(i) == 'M'){
-				ans += 1000;
-			}
-			else if(text.charAt(i) == 'D'){
-				ans += 500;
-			}
-			else if (text.charAt(i) == 'C'){
-				if(text.charAt(i + 1) == 'M'){
-					ans -= 100;
-				}else{
-					ans += 100;
-				}
-			}
-			else if(text.charAt(i) == 'L'){
-				ans += 50;
-			}
-			else if (text.charAt(i) == 'X'){
-				if (text.charAt(i + 1) == 'C'){
-					ans -= 10;
-				}else{
-					ans += 10;
-				}
-			}
-			else if (text.charAt(i) == 'V'){
-				ans += 5;
-			}
-			else if (text.charAt(i) == 'I'){
-				if (text.charAt(i + 1) == 'V'){
-					ans -= 1;
-				}
-				else if (text.charAt(i + 1) == 'X'){
-					ans -= 1;
-				}else{
-					ans += 1;
-				}
-			}
-		}
-		println("the integer is: " + ans);
+public class jsjs extends ConsoleProgram{
+	public void run(){
+		int x = 2;
+		int y = 3;
+		GOval o = new GOval(4,3);	//width, height
+		println(racxa(x,2,o));
+		println(racxa(y,x/2+x,o));
 	}
+	
+	public int racxa(int x, int y, GOval o){
+		String s = "hello world";
+		x = varesiRacxa(x/y, x+(int)o.getHeight(), s);
+		o.scale(2);
+		y = varesiRacxa(x, y+(int)o.getHeight(), s);
+		return x+y;
+	}
+	
+	public int varesiRacxa(double x, int y, String s){
+		int z = (int)x + s.charAt(0)-s.charAt(1);
+		s = s.substring(1);
+		return z + y;
+	}	
 }
