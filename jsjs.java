f2f3289096cf57394ce012dd4bf220c654ee2fd1
S <s>
2023-11-22 13:17:02 +0400

    {"types":"C","runs":19}

--- jsjs.java
+++ jsjs.java
@@ -13,24 +13,29 @@ public class jsjs extends GraphicsProgram {
 	private int prevY;
 	
 	public void run(){
-		print(a());
 		addMouseListeners();
 		circle = new GOval (2 * RADIUS, 2 * RADIUS);
 		circle.setFilled(true);
-//		add(circle, getWidth() / 2 - RADIUS, getHeight() / 2 - RADIUS);
+		add(circle, getWidth() / 2 - RADIUS, getHeight() / 2 - RADIUS);
 	}
 	public void mousePressed(MouseEvent e){
 		 GObject obj = getElementAt(e.getX(), e.getY());
 		
+		 if(obj != null){
+			 prevX = e.getX();
+			 prevY = e.getY();
+		 }
 	}
-private int a(){
-	int x = 7+12;
-	return x;
-}
+	
 
 	public void mouseDrugged(MouseEvent e){
+		GObject obj = getElementAt(e.getX(), e.getY());
 		
+		 if(obj != null){
+			 obj.move(prevX - RADIUS, prevY - RADIUS);
+			 
+			 
+		 }
 		
-
 	}
 }
