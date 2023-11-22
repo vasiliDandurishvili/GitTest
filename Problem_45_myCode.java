eb39057d877e396eb478169bd7dd4642da9a2377
S <s>
2023-11-22 17:06:42 +0400

    {"types":"A","runs":6}

---
+++ Problem_45_myCode.java
@@ -0,0 +1,30 @@
+import acm.graphics.*;
+import acm.program.*;
+import acm.util.*;
+
+import java.applet.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Problem_45_myCode extends ConsoleProgram {
+	private int numOfSym = 0;
+	public void run(){
+	String text = readLine("enter text: ");
+	String symbolText = readLine("enter char: ");
+	
+	char symbol = symbolText.charAt(0);
+			
+	int finalSum = symbInText(text, symbol);
+	print ("sum of the " + symbol + " sybmbol: " + finalSum);
+	}
+	
+	private int symbInText(String text, char symbol) {
+		for (int i = 0; i < text.length(); i++){
+			if(text.charAt(i) == symbol){
+				numOfSym++;
+			}
+		}
+		return numOfSym;
+	}
+
+}
