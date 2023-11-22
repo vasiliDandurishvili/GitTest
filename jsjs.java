08cbb28e8e27fe8a43005fec9c0b12e40a580650
S <s>
2023-11-22 16:58:00 +0400

    {"types":"C","runs":4}

--- jsjs.java
+++ jsjs.java
@@ -6,26 +6,26 @@ import java.applet.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class jsjs extends GraphicsProgram {
-	private static final double RADIUS = 30;
-	private static final double vy = 1;
-	private static final double PAUSE = 8;
-	private GOval circle;
-
-	public void run() {// removeAll();
-		circle = new GOval(2 * RADIUS, 2 * RADIUS);
-		circle.setFilled(true);
-
-		addMouseListeners();
-		while (true) {
-			circle.move(0, vy);
-			pause(PAUSE);
-		}
-
+public class jsjs extends ConsoleProgram {
+	private int numOfSym = 0;
+	public void run(){
+	String text = readLine("enter text: ");
+	String symbolText = readLine("enter char: ");
+	
+	char symbol = symbolText.charAt(0);
+			
+	int finalSum = symbInText(text, symbol);
+	print ("sum of the " + symbol + " sybmbol: " + finalSum);
 	}
-
-	public void mouseClicked(MouseEvent e) {
-		add(circle, e.getX() - RADIUS, e.getY() - RADIUS);
+	
+	private int symbInText(String text, char symbol) {
+		for (int i = 0; i < text.length(); i++){
+			if(text.charAt(i) == symbol){
+				numOfSym++;
+			}
+			return numOfSym;
+		}
+		return 0;
 	}
 
 }
