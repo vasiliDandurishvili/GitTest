e3510d7d5e60e53e23677859559d33ae94f0ef7f
S <s>
2023-11-22 18:19:47 +0400

    {"types":"C","runs":6}

--- jsjs.java
+++ jsjs.java
@@ -7,24 +7,22 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class jsjs extends ConsoleProgram {
-	private int numOfSym = 0;
-	public void run(){
-	String text = readLine("enter text: ");
-	String symbolText = readLine("enter char: ");
-	
-	char symbol = symbolText.charAt(0);
-			
-	int finalSum = symbInText(text, symbol);
-	print ("sum of the " + symbol + " sybmbol: " + finalSum);
-	}
-	
-	private int symbInText(String text, char symbol) {
-		for (int i = 0; i < text.length(); i++){
-			if(text.charAt(i) == symbol){
-				numOfSym++;
+	private int mostChar = 0;
+
+	public void run() {
+		String text = readLine("enter text: ");
+
+		for (int i = 0; i < text.length(); i++) {
+			 int currNum = 0;
+			for (int j = 0; j < text.length(); j++) {
+				if(text.charAt(i) == text.charAt(j)){
+					currNum++;
+				}
+			}
+			if(currNum > mostChar){
+				mostChar = currNum;
+				print("mostlu char: " + text.charAt(i) );
 			}
 		}
-		return numOfSym;
 	}
-
 }
