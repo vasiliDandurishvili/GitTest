7f2811713c8d34391b500539ea394411fe0980ce
S <s>
2023-11-22 18:52:38 +0400

    {"types":"C","runs":18}

--- Problem46_myCode.java
+++ Problem46_myCode.java
@@ -1,4 +1,30 @@
+import acm.graphics.*;
+import acm.program.*;
+import acm.util.*;
 
-public class Problem46_myCode {
+import java.applet.*;
+import java.awt.*;
+import java.awt.event.*;
 
+public class Problem46_myCode extends ConsoleProgram {
+	private int mostChar = 0;
+	private char mostC;
+
+	public void run() {
+		String text = readLine("enter text: ");
+
+		for (int i = 0; i < text.length(); i++) {
+			int currNum = 0;
+			for (int j = 0; j < text.length(); j++) {
+				if (text.charAt(i) == text.charAt(j)) {
+					currNum++;
+				}
+			}
+			if (currNum > mostChar) {
+				mostChar = currNum;
+				mostC = text.charAt(i);
+			}
+		}
+		println("most offen char: " + mostC + " the number is: " + mostChar);
+	}
 }
