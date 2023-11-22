97534f805304712c9c044a65f56994ac017e8354
S <s>
2023-11-22 22:38:01 +0400

    {"types":"C","runs":23}

--- Problem_47_myCode.java
+++ Problem_47_myCode.java
@@ -1,4 +1,29 @@
+import acm.graphics.*;
+import acm.program.*;
+import acm.util.*;
 
-public class Problem_47_myCode {
+import java.applet.*;
+import java.awt.*;
+import java.awt.event.*;
 
+public class Problem_47_myCode extends ConsoleProgram {
+
+	public void run() {
+		String text = readLine("enter text: ");
+		boolean isPalindrom = isPalindrome(text);
+		if(isPalindrom){
+			print(text + " is palindrom.");
+		}else{
+			print(text + " is not palindrom.");
+		}
+	}
+
+	private boolean isPalindrome(String text) {
+		for (int i = 0; i < text.length() / 2; i++){
+			if(text.charAt(i) != text.charAt(text.length() - i - 1 )){
+				return false;
+			}
+		}
+		return true;
+	}
 }
