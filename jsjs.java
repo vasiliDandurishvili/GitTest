5e505774cfa67be5d56c0d2bd519d63a3f6e32c6
S <s>
2023-11-25 11:52:16 +0400

    {"types":"C","runs":1}

--- jsjs.java
+++ jsjs.java
@@ -5,11 +5,23 @@ import acm.util.*;
 import java.applet.*;
 import java.awt.*;
 import java.awt.event.*;
-public class jsjs extends ConsoleProgram{
+public class jsjs extends GraphicsProgram{
 	
 	public void run(){
-		String text = readLine("enter text: ")
+		createGrects();
+		addMouseListeners();
 		
+	}
+
+	private void createGrects() {
+		for (int i = 0; i < 10; i++){
+			for(int j = 0; j < 10; j++){
+				GRect brick = new GRect (getWidth() / 10, getHeight() /10);
+				double x = j * getWidth() / 10;
+				double y = i * getHeight() /10;
+				add(brick, x, y);
+			}
+		}
 		
 	}
 }
