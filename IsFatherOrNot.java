06625476cf21f7c555dd4f8920be20e7c7ca0199
S <s>
2023-11-25 15:01:40 +0400

    {"types":"C","runs":36}

--- IsFatherOrNot.java
+++ IsFatherOrNot.java
@@ -1,43 +1,44 @@
 import acm.program.ConsoleProgram;
 
 public class IsFatherOrNot extends ConsoleProgram {
-	private int mostCommon = 0;
-	private int currCommon ;
-	public void run(){
-		String fatherDNM = readLine("father's DNM: ");
-		String childDNM = readLine("child's DNM: ");
-		
-		boolean isFather = isFather(fatherDNM, childDNM);
-		if(isFather){
-			print("true");
-		}else{
-			print("false");
-		}
-	}
-	private Boolean isFather(String fatherDNM,String childDNM){
-		for(int i = 0; i < childDNM.length(); i++){
-			currCommon = 0;
-			for(int j = 0; j < fatherDNM.length(); j ++){
-				if(childDNM.charAt(i) ==fatherDNM.charAt(j)){
-					while(childDNM.charAt(i)  == fatherDNM.charAt(j)){
-						currCommon++;
-						i++;
-						j++;
-					}
-					i = i - currCommon;
-					j = j - currCommon;
-					if(currCommon > mostCommon ){
-						mostCommon = currCommon;
-					}
-				}
-			}
-			
-			
-		}
-		if(mostCommon > fatherDNM.length() / 2){
-			return true;
-		}else{
-			return false;	
-		}
-	}
+    private int mostCommon = 0;
+    private int currCommon;
+
+    public void run() {
+        String fatherDNM = readLine("father's DNM: ");
+        String childDNM = readLine("child's DNM: "); // Fix: Change to "child's DNM"
+
+        boolean isFather = isFather(fatherDNM, childDNM);
+        if (isFather) {
+            print("true");
+        } else {
+            print("false");
+        }
+    }
+
+    private Boolean isFather(String fatherDNM, String childDNM) {
+        for (int i = 0; i < childDNM.length(); i++) {
+            currCommon = 0;
+            for (int j = 0; j < fatherDNM.length(); j++) {
+                if (childDNM.charAt(i) == fatherDNM.charAt(j)) {
+                    while (i < childDNM.length() && j < fatherDNM.length() &&
+                            childDNM.charAt(i) == fatherDNM.charAt(j)) {
+                        currCommon++;
+                        i++;
+                        j++;
+                    }
+                    i--; // Adjust i to the correct position after the loop
+                    j--; // Adjust j to the correct position after the loop
+                    if (currCommon > mostCommon) {
+                        mostCommon = currCommon;
+                    }
+                }
+            }
+        }
+        if (mostCommon > fatherDNM.length() / 2) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
