481ec699ff10ca3bb028d4c2bb56d07378439793
S <s>
2023-11-25 15:09:36 +0400

    {"types":"C","runs":46}

--- IsFatherOrNot.java
+++ IsFatherOrNot.java
@@ -2,42 +2,43 @@ import acm.program.ConsoleProgram;
 
 public class IsFatherOrNot extends ConsoleProgram {
 	private int mostCommon = 0;
-	private int currCommon ;
-	public void run(){
+	private int currCommon;
+
+	public void run() {
 		String fatherDNM = readLine("father's DNM: ");
 		String childDNM = readLine("child's DNM: ");
-		
+
 		boolean isFather = isFather(fatherDNM, childDNM);
-		if(isFather){
+		if (isFather) {
 			print("true");
-		}else{
+		} else {
 			print("false");
 		}
 	}
-	private Boolean isFather(String fatherDNM,String childDNM){
-		for(int i = 0; i < childDNM.length(); i++){
+
+	private Boolean isFather(String fatherDNM, String childDNM) {
+		for (int i = 0; i < childDNM.length(); i++) {
 			currCommon = 0;
-			for(int j = 0; j < fatherDNM.length(); j ++){
-				if(childDNM.charAt(i) ==fatherDNM.charAt(j)){
-					while(i < childDNM.length() && j < fatherDNM.length() && childDNM.charAt(i)  == fatherDNM.charAt(j)){
+			for (int j = 0; j < fatherDNM.length(); j++) {
+				if (childDNM.charAt(i) == fatherDNM.charAt(j)) {
+					while (i < childDNM.length() && j < fatherDNM.length()
+							&& childDNM.charAt(i) == fatherDNM.charAt(j)) {
 						currCommon++;
 						i++;
 						j++;
 					}
 					i = i - 1; // while is mere swori pozicia.
 					j = j - 1; // while is mere swori pozicia.
-					if(currCommon > mostCommon ){
+					if (currCommon > mostCommon) {
 						mostCommon = currCommon;
 					}
 				}
 			}
-			
-			
 		}
-		if(mostCommon > fatherDNM.length() / 2){
+		if (mostCommon > fatherDNM.length() / 2) {
 			return true;
-		}else{
-			return false;	
+		} else {
+			return false;
 		}
 	}
 }
