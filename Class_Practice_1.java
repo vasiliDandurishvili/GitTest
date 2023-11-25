871a14595b3a130dd93066d47225e1354570a682
S <s>
2023-11-25 14:42:44 +0400

    {"types":"C","runs":33}

--- Class_Practice_1.java
+++ Class_Practice_1.java
@@ -6,17 +6,20 @@ import acm.program.GraphicsProgram;
 
 public class Class_Practice_1 extends ConsoleProgram {
 	public void run(){
-		for(int i = 0; i < 5; i++){
-			int a = 5;
-			while(true){
-				println(i);
-				i++;
-				if(i == 10){
-					break;
-				}
-			}
-			println(i);
-		}
+		String str1 = "mari";
+		String str2 = "mari";
+		String str3 = new String ("mari");
+		String str4 = readLine("enter str4: ");
+		
+//		//jer gaaketebs mimatebas orive mxares da mere sheadarebs + > ==(prioritetulia).
+//		println("1 ,2" + str1 == str2 + "" +str1.equals(str2));
+//		println("1 ,3" + str1 == str3 + "" +str1.equals(str3));
+//		println("1 ,4" + str1 == str4 + "" +str1.equals(str4));
+		
+		
+		println("1 ,2 " + (str1 == str2) + " " +str1.equals(str2));
+		println("1 ,3 " + (str1 == str3) + " " +str1.equals(str3));
+		println("1 ,4 " + (str1 == str4) + " " +str1.equals(str4));
 	}
 	
 }
