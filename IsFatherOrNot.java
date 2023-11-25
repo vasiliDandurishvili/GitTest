a25bebf81a77c7d2ff038855528805489123e9ea
S <s>
2023-11-25 14:41:04 +0400

    {"types":"C","runs":30}

--- IsFatherOrNot.java
+++ IsFatherOrNot.java
@@ -1,6 +1,8 @@
 import acm.program.ConsoleProgram;
 
 public class IsFatherOrNot extends ConsoleProgram {
+	private int mostCommon = 0;
+	private int currCommon ;
 	public void run(){
 		String fatherDNM = readLine("father's DNM: ");
 		String childDNM = readLine("father's DNM: ");
@@ -14,8 +16,15 @@ public class IsFatherOrNot extends ConsoleProgram {
 	}
 	private Boolean isFather(String fatherDNM,String childDNM){
 		for(int i = 0; i < childDNM.length(); i++){
+			currCommon = 0;
 			for(int j = 0; j < fatherDNM.length(); j ++){
-				
+				if(childDNM.charAt(i) ==fatherDNM.charAt(j)){
+					currCommon++;
+					while(childDNM.charAt(i +1)  == fatherDNM.charAt(j +1)){
+						currCommon++;
+						i
+					}
+				}
 			}
 			
 		}
