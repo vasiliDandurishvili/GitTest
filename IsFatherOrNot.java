4c8360a4a23bd5ee224cb5c526edd64f931af7de
S <s>
2023-11-25 13:58:43 +0400

    {"types":"C","runs":28}

--- IsFatherOrNot.java
+++ IsFatherOrNot.java
@@ -1,4 +1,15 @@
+import acm.program.ConsoleProgram;
 
-public class IsFatherOrNot {
-
+public class IsFatherOrNot extends ConsoleProgram {
+	public void run(){
+		String fatherDNM = readLine("father's DNM: ");
+		String childDNM = readLine("father's DNM: ");
+		
+		boolean isFather = isFather(fatherDNM, childDNM);
+		if(isFather){
+			print("true");
+		}else{
+			print("false")
+		}
+	}	
 }
