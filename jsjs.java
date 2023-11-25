7f17692ae4b3287f435f352a20a95aecf9aa3c9a
S <s>
2023-11-25 22:14:04 +0400

    {"types":"C","runs":67}

--- jsjs.java
+++ jsjs.java
@@ -3,7 +3,7 @@ import acm.program.ConsoleProgram;
 public class jsjs extends ConsoleProgram{
 
 	public void run(){
-		print('d' + 1 - 'a' + "E" + 3 + 2 + 'ბ' );
+		print('d' + 1 - 'a' + "E" + 3 + 2 + 'b' );
 	}	
 
 }
