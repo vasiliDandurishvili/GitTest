6294a02997140b3c3e52190b907902bbb0c29550
S <s>
2023-11-25 15:42:29 +0400

    {"types":"C","runs":59}

--- jsjs.java
+++ jsjs.java
@@ -1,39 +1,12 @@
-import acm.graphics.*;
-import acm.program.*;
-import acm.util.*;
+import acm.program.ConsoleProgram;
 
-import java.applet.*;
-import java.awt.*;
-import java.awt.event.*;
-public class jsjs extends GraphicsProgram{
-	private GRect brick;
-	private int currpaints = 0;
-	private int maxPaints = 2;
+public class jsjs extends ConsoleProgram{
+
 	public void run(){
-		addMouseListeners();
-
-		createGrects();
-		setBackground(Color.YELLOW);
+		String s = "abcdefgh";
+		s.substring(0, 11);
 		
+		println(s);
 	}
 
-	private void createGrects() {
-		for (int i = 0; i < 10; i++){
-			for(int j = 0; j < 10; j++){
-				brick  = new GRect (40 , 30);
-				brick.setFilled(true);
-				brick.setColor(Color.WHITE);
-				double x = j * 40 + j * 5;
-				double y = i * 30 + i * 5;
-				add(brick, x, y);
-			}
-		}
-		
-	}
-	public void mouseClicked(MouseEvent e){
-		if(getElementAt(e.getX(), e.getY()) == brick){
-			brick.setColor(Color.BLACK);
-			
-		}
-	}
 }
