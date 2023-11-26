243a02e42a44019a13a4e9eac9663276f0eaf7e7
S <s>
2023-11-26 12:28:33 +0400

    {"types":"C","runs":79}

--- jsjs.java
+++ jsjs.java
@@ -5,24 +5,26 @@ import java.awt.Color;
 import acm.graphics.GOval;
 import acm.program.*;
 
-public class jsjs extends GraphicsProgram{
+public class jsjs extends GraphicsProgram {
 
-	public void run(){
-			char a = 'a';
-			int num = 5;
-			GOval circle = new GOval(100, 100);
-			String dukesPwd = "iloveCS106A";
-			a = bark(num);
-			squeak(circle, dukesPwd);
-			println("all done!");
-			}
-			private char bark(int num){
-			GOval circle = new GOval(150, 150);
-			num += 25;
-			return 'b';
-			}
-			private void squeak(GOval oval, String word){
-			oval.setColor(Color.BLUE);
-			word.toUpperCase();
-			}
-	}	
+	public void run() {
+		char a = 'a';
+		int num = 5;
+		GOval circle = new GOval(100, 100);
+		String dukesPwd = "iloveCS106A";
+		a = bark(num);
+		squeak(circle, dukesPwd);
+		println("all done!");
+	}
+
+	private char bark(int num) {
+		GOval circle = new GOval(150, 150);
+		num += 25;
+		return 'b';
+	}
+
+	private void squeak(GOval oval, String word) {
+		oval.setColor(Color.BLUE);
+		word.toUpperCase();
+	}
+}
