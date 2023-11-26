caa8643be0c7237280177b574061d86ebb133aa0
S <s>
2023-11-26 17:08:38 +0400

    {"types":"C","runs":88}

--- jsjs.java
+++ jsjs.java
@@ -2,39 +2,21 @@ import acm.program.ConsoleProgram;
 import acm.util.RandomGenerator;
 
 public class jsjs extends ConsoleProgram {
-	private RandomGenerator rgen = RandomGenerator.getInstance();
-	private int sumOfTreats = 0;
-
+	
 	public void run() {
-		println("welcome to our dog opening! ");
-		println("             ");
-		cycleForDogs();
-		println("At the Grand Opening, there will be " + sumOfTreats + " dog treats.");
+	String text1 = readLine("enter first text: ");
+	String text2 = readLine("enter second text: ");
+	
+	String samePart = samePart(text1, text2);
+	
+	String leftPart1 = text1.substring(samePart.length());
+	String leftPart2 = text2.substring(samePart.length());
+	
+	println(samePart.length() + samePart + "*" +leftPart1.length() + leftPart1 + leftPart2.length() + leftPart2);
 	}
-	private void cycleForDogs(){
-		while(true){
-			int luckyNum = rgen.nextInt(1,4);
-			
-			String dogName = readLine("what is your dog's name: ");
-			if(dogName.equals("")){
-				break;
-			}
-			int dogSize = readInt("how big is your dog? Enter a num: 1 - small, 2 - medium, 3 - big: ");
-			if (dogSize == 1){
-				println("oh boy! " + dogName + " gets 2 dog treats!");
-				sumOfTreats += 2;
-			}
-			else if (dogSize == 2){
-				println("oh boy! " + dogName + " gets 4 dog treats!");
-				sumOfTreats += 4;
-			}
-			else if (dogSize == 3){
-				println("oh boy! " + dogName + " gets 6 dog treats!");
-				sumOfTreats += 6;
-			}
-			if (luckyNum == 1){
-				println(dogName + " also won asqueaky toy");
-			}
-		}
+
+	private String samePart(String text1, String text2) {
+		String comm =
+		
 	}
 }
