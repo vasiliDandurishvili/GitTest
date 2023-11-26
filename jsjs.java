d501f79f243f41311d4305f3ea28eb9994898b71
S <s>
2023-11-26 12:47:16 +0400

    {"types":"C","runs":81}

--- jsjs.java
+++ jsjs.java
@@ -1,12 +1,21 @@
 import acm.program.ConsoleProgram;
+import acm.util.RandomGenerator;
 
 public class jsjs extends ConsoleProgram {
+	private RandomGenerator rgen = RandomGenerator.getInstance();
 	private int sumOfTreats = 0;
 
 	public void run() {
 		println("welcome to our dog opening! ");
 		println("             ");
-
+		cycleForDogs();
+		println("At the Grand Opening, there will be" + sumOfTreats + " dog treats.");
 	}
-
+	private void cycleForDogs(){
+		while(true){
+			String dogName = readLine("what is your dog's name: ");
+			
+		}
+		
+	}
 }
