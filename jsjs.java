65d824033e348ca082aeb16a15c13c1f1e4891c3
S <s>
2023-11-28 16:32:58 +0400

    {"types":"C","runs":104}

--- jsjs.java
+++ jsjs.java
@@ -49,6 +49,13 @@ public class jsjs extends GraphicsProgram {
 		GObject obj1 = getElementAt(e.getX(), e.getY());
 		}else if (obj1 != null && obj2 == null){
 			GObject obj2 = getElementAt(e.getX(), e.getY());
+			
+			if(obj1.getColor().equals(obj2.getColor())){
+				remove(obj1);
+				remove(obj2);
+				
+				
+			}
 		}
 		
 		
