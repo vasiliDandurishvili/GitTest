36d20b6ec4127370b925650de420c573fb692c32
S <s>
2023-11-28 10:53:20 +0400

    {"types":"C","runs":98}

--- jsjs.java
+++ jsjs.java
@@ -1,9 +1,36 @@
-import acm.program.ConsoleProgram;
+import java.awt.Color;
+
+import acm.graphics.*;
+import acm.program.*;
 import acm.util.RandomGenerator;
 
-public class jsjs extends ConsoleProgram {
-	
+public class jsjs extends GraphicsProgram {
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	private static final int NUM_COLUMNS = 5;
+	private static final int NUM_ROW = 5;
 	public void run() {
+		addMouseListeners();
+		drawSquares();
+	}
+	private void drawSquares() {
+		for(int i = 0; i < NUM_COLUMNS; i++){
+			for(int j = 0; j < NUM_ROW; j++){
+				GRect rect = new GRect (getWidth() / NUM_COLUMNS, getHeight() / NUM_ROW);
+				rect.setFilled(true);
+				 Color fillColor = getRandomColor();
+				rect.setFillColor(fillColor);
+				Color frameColor = getRandomColor();
+				rect.setColor(frameColor);
+				add(rect,getWidth() / NUM_COLUMNS * i,getHeight() / NUM_ROW * j );
+			}
+		}
 		
 	}
+	 private Color getRandomColor() {
+	        int red = rgen.nextInt(256);
+	        int green = rgen.nextInt(256);
+	        int blue = rgen.nextInt(256);
+	        int yellow = rgen.nextInt(256);
+	        return new Color(red, green, blue, yellow);
+	    }
 }
