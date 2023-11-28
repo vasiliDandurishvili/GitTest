32db19c2bafd5bea711e31126a0d474435914fb4
S <s>
2023-11-28 17:34:53 +0400

    {"types":"C","runs":128}

--- Problem_51.java
+++ Problem_51.java
@@ -9,6 +9,8 @@ public class Problem_51 extends ConsoleProgram {
 	public void run() {
 		String text = readLine("Enter text: ");
 		String replaced = removeDuplicates(text);
+		
+		println(replaced);
 	}
 	private String removeDuplicates(String text){
 		text.replace("i","");
