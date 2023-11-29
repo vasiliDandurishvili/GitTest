36f63ddbe72549019b090cd07fb90e666dfd8636
S <s>
2023-11-29 12:00:01 +0400

    {"types":"C","runs":136}

--- Midterm_geometric_progression.java
+++ Midterm_geometric_progression.java
@@ -1,4 +1,38 @@
+import acm.program.ConsoleProgram;
 
-public class Midterm_geometric_progression {
-
+public class Midterm_geometric_progression extends ConsoleProgram {
+	public void run() {
+		boolean check = true;
+		int num1 = readInt("enter num: ");
+		if (num1 == -1) {
+			println("Not a geometric progression.");
+		}
+		int num2 = readInt("enter num: ");
+		if (num2 == -1) {
+			println("Not a geometric progression.");
+		}
+		int num3 = readInt("enter num: ");
+		if (num3 == -1) {
+			println("Not a geometric progression.");
+		}
+		double q = num2 / (double) num1;
+		while (true) {
+			int numX = readInt("enter num: ");
+			if (numX == num3 * q) {
+				num3 = numX;
+				continue;
+			}
+			if (numX != num3 * q && numX != -1){
+				check = false;
+			}
+			if(numX == -1){
+				break;
+			}
+		}
+		if(check){
+			println("Geometric progresion");
+		}else {
+			println("Not a geometric progression.");
+		}
+	}
 }
