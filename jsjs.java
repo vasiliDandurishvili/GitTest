d3de104454dc01fc428836f9d1fd16bf40c938f5
S <s>
2023-12-01 22:17:59 +0400

    {"types":"C","runs":231}

--- jsjs.java
+++ jsjs.java
@@ -1,10 +1,19 @@
+import java.awt.event.MouseEvent;
+
+import acm.graphics.GLine;
 import acm.program.*;
 
-public class jsjs extends ConsoleProgram {
-
+public class jsjs extends GraphicsProgram {
+ private int xs;
+ private int ys;
+ private GLine line;
+ 
 	public void run() {
-		String s = "how are you how are you how";
-		print(s.replace('h', 'c'));
-		
+		xs = ys = 0;
+		addMouseListeners();
 	}	
+	public void mousePressed (MouseEvent e){
+		line = new GLine(xs, ys, e.getX(), e.getY());
+		add(line);
+	}
 }
