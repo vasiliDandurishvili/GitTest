3be4834a5d8b743e5eacaadcb231ed09895c70bc
S <s>
2023-12-01 11:44:11 +0400

    {"types":"C","runs":157}

--- jsjs.java
+++ jsjs.java
@@ -7,64 +7,75 @@ import acm.util.RandomGenerator;
 
 public class jsjs extends GraphicsProgram {
 	private RandomGenerator rgen = RandomGenerator.getInstance();
-	private static final int NUM_COLUMNS = 5;
-	private static final int NUM_ROW = 5;
-	private GObject obj1;
-	private GObject obj2;
+	private GOval ball1;
+	private GOval ball2;
+	private static final double BALL_RADIUS = 20;
 
 	public void run() {
 		addMouseListeners();
-		drawSquares();
+		drawBalls();
+		
 	}
 
-	private void drawSquares() {
-		for (int i = 0; i < NUM_COLUMNS; i++) {
-			for (int j = 0; j < NUM_ROW; j++) {
-				GRect rect = new GRect(getWidth() / NUM_COLUMNS, getHeight() / NUM_ROW);
-				rect.setFilled(true);
-				Color fillColor = getRandomColor();
-				rect.setFillColor(fillColor);
-				Color frameColor = getRandomColor();
-				rect.setColor(frameColor);
-				add(rect, getWidth() / NUM_COLUMNS * i, getHeight() / NUM_ROW * j);
-			}
+	public void mouseClicked(MouseEvent e) {
+
+	}
+	
+	
+	
+	
+	
+	private void drawBalls(){
+		for(int i = 0; i < 25; i++){
+			ball1 = new GOval (2 * BALL_RADIUS, 2 * BALL_RADIUS);
+			ball1.setFilled(true);
+			double x1 = rgen.nextDouble(0, getWidth() - 2 * BALL_RADIUS);
+			double y1 = rgen.nextDouble(0, getHeight() - 2 * BALL_RADIUS);
+			Color ball1Color = getRandomColor();
+			ball1.setColor(ball1Color);
+			
+			ball2 = new GOval (2 * BALL_RADIUS, 2 * BALL_RADIUS);
+			ball2.setFilled(true);
+			
+			double x2 = rgen.nextDouble(0, getWidth() - 2 * BALL_RADIUS);
+			double y2 = rgen.nextDouble(0, getHeight() - 2 * BALL_RADIUS);
+			Color ball2Color = getRandomColor();
+			ball1.setColor(ball2Color);
+			
+			add(ball1, x1, y1);
+			add(ball2,x2,y2);
 		}
-
 	}
-
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	private Color getRandomColor() {
-		int a = rgen.nextInt(1, 3);
+		int a = rgen.nextInt(1, 5);
 		if (a == 1) {
 			return Color.YELLOW;
 		} else if (a == 2) {
 			return Color.BLUE;
 		} else if (a == 3) {
 			return Color.GREEN;
-		} else {
-			return Color.BLACK; // TO GET ERROR.
-		}
-	}
-
-	public void mouseClicked(MouseEvent e) {
-		if (obj1 == null && obj2 == null) {
-			obj1 = getElementAt(e.getX(), e.getY());
-
-		} else if ((obj1 != null) && (obj2 == null) && (getElementAt(e.getX(), e.getY()) != obj1)) {
-			// tu isev obj1-s ar daaklika.
-			obj2 = getElementAt(e.getX(), e.getY());
-
-			if (((GRect) obj1).getColor().equals(((GRect) obj2).getColor())
-					&& ((GRect) obj1).getFillColor().equals(((GRect) obj2).getFillColor())) {
-				remove(obj1);
-				remove(obj2);
-
-				// Reset obj1 and obj2 for the next comparison
-				obj1 = null;
-				obj2 = null;
-			}
+		}else if (a == 4) {
+			return Color.PINK;
+		}else if (a == 3) {
+			return Color.CYAN;	
 		}else{
-			obj1 = null;
-			obj2 = null;
+			return Color.BLACK;
 		}
 	}
 
