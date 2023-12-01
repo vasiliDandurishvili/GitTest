549498f601ece4fe7c2d5f87c36d832c8c25b46f
S <s>
2023-12-01 11:49:41 +0400

    {"types":"C","runs":163}

--- jsjs.java
+++ jsjs.java
@@ -32,7 +32,6 @@ public class jsjs extends GraphicsProgram {
 			double x1 = rgen.nextDouble(0, getWidth() - 2 * BALL_RADIUS);
 			double y1 = rgen.nextDouble(0, getHeight() - 2 * BALL_RADIUS);
 			Color ball1Color = getRandomColor();
-			System.out.println(ball1Color);
 			ball1.setColor(ball1Color);
 			
 			ball2 = new GOval (2 * BALL_RADIUS, 2 * BALL_RADIUS);
