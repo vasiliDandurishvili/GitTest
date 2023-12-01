b0871e220fe4e1e55fe9520bfa4c0561d0312096
S <s>
2023-12-01 22:05:08 +0400

    {"types":"C","runs":228}

--- jsjs.java
+++ jsjs.java
@@ -3,34 +3,8 @@ import acm.program.*;
 public class jsjs extends ConsoleProgram {
 
 	public void run() {
-		String text = readLine("enter text: ");
-		String cleared = clearText(text);
-
-		println(cleared);
+		String s = "how are you how are you how";
+		print(s.replace("how", "xxx"));
 		
-	}
-
-	private String clearText(String text) {
-		  String result = text;
-
-	        while (true) {
-	            String previousResult = result;
-
-	            for (int i = 0; i < result.length() - 1; i++) {
-	                if (result.charAt(i) == result.charAt(i + 1)) {
-	                    String sub1 = result.substring(0, i);
-	                    String sub2 = result.substring(i + 2);
-	                    result = sub1 + sub2;
-	                    i--; // Check the same index again after removing the duplicate
-	                }
-	            }
-
-	            if (previousResult.equals(result)) {
-	                break; // No changes were made in the entire iteration
-	            }
-	        }
-		return result;
-		}
-
-	
+	}	
 }
