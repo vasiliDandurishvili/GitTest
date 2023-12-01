6cc298fd3d7cc6f23b9494bd71f67903f544ffba
S <s>
2023-12-01 13:08:38 +0400

    {"types":"C","runs":172}

--- jsjs.java
+++ jsjs.java
@@ -5,24 +5,25 @@ public class jsjs extends ConsoleProgram {
 	
 	public void run() {
 	int n = readInt("enter n: ");
-	int k = readInt("enter k: ");
-	
-	String text = buildString(n,k);
-	
-	
-	println(text);
+	int m= readInt("enter m: ");
+	// m > n
+	println(log(2,5));
+	println(log(3,29));
+	println(log(5,88));
 	}
-		private String buildString(int n, int k){
-			String result ="";
-			
-			
-			
-			
-//			result = ch + result;
-			
-			return result;
+	private int log(int n, int m){
+		int result = 0;
+		while(true){
+			int sum = m / n;
+			m = m / n;
+			if(sum > 0){
+				result++;
+			}else{
+				break;
+			}
 		}
-	
+		return result;
+	}
 
 	
 }
