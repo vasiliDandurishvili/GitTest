657efb6af9c5ba131376924f6df48d907ea7c8a5
S <s>
2023-12-02 13:30:52 +0400

    {"types":"C","runs":6}

--- jsjs.java
+++ jsjs.java
@@ -1,27 +1,48 @@
-import java.awt.event.MouseEvent;
+//48. კონსოლიდან შეგყვავს ტექსტი პროგრამამ უნდა დაბეჭდოს სტატისტიკა რომელი 
+//სიმბოლო რამდენჯერ გვხვდება
 
-import acm.graphics.GLine;
-import acm.program.*;
+import acm.program.ConsoleProgram;
 
-public class jsjs extends ConsoleProgram {
-private char X;
+public class jsjs extends ConsoleProgram{
 	public void run() {
-		String s = readLine("enter: ");
-		while (!s.equals("")) {
-			int amount = 0;
-			for (int i = 0; i < s.length(); i++) {
-				X = s.charAt(i);
-				for (int j = 0; j < s.length(); j++) {
-					if (s.charAt(j) == X) {
-						amount++;
-						String delete = "" + X;
-						String newS = s.replace(delete, "");
-						s = newS;
-					}
-				}
+		String text = readLine("Enter the text to count statistics: ");
+		printCharStatistics(text);
+	}
+	
+	private void printCharStatistics(String text) {
+		for(int i = 0; i < text.length(); i++) {
+			char currCh = text.charAt(i);
+			if(isFirstOccurence(text, i)) {
+				int count = countSymbol(text, currCh);
+				println(currCh + ": " + count);
 			}
-			println("num of: " + X + " : " + amount);
-		}
+		}	
 	}
 
+//	mariami index = 4
+	private boolean isFirstOccurence(String text, int index) {
+		char ch = text.charAt(index); // 'a'
+		int firstIndex = text.indexOf(ch); // 1
+		return firstIndex == index;
+	}
+	
+//	mariami 'a'
+//	private boolean isFirstOccurence(String text, int index) {
+//		for(int i = 0; i < index; i++) {
+//			if(text.charAt(i) == text.charAt(index)) {
+//				return false;
+//			}
+//		}
+//		return true;
+//	}
+
+	private int countSymbol(String text, char symbol) {
+		int symbolCount = 0;
+		for(int  i = 0; i < text.length(); i++) {
+			if(text.charAt(i) == symbol) {
+				symbolCount++; // symbolCount = symbolCount + 1;
+			}
+		}
+		return symbolCount;
+	}
 }
