be54165931fea4f0966327de4d4314101a848566
S <s>
2023-12-02 13:55:19 +0400

    {"types":"C","runs":11}

--- Problem_34.java
+++ Problem_34.java
@@ -15,10 +15,11 @@ private RandomGenerator rgen = RandomGenerator.getInstance();
 private static final int RADIUS_MIN = 10;
 private static final int RADIUS_MAX = 30;
 private GOval oval;
-private int radius = rgen.nextInt(RADIUS_MIN,RADIUS_MAX);
+private int radius;
 	public void run(){
 	addMouseListeners();
 	while(true){
+	radius = rgen.nextInt(RADIUS_MIN,RADIUS_MAX);
 	oval = new GOval (2 * radius,2 * radius );
 	oval.setFilled(true);
 	oval.setColor(rgen.nextColor());
