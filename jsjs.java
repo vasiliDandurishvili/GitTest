31bee5fd95de3c5a50517329959afa772ff6a09b
S <s>
2023-12-02 12:48:45 +0400

    {"types":"C","runs":1}

--- jsjs.java
+++ jsjs.java
@@ -7,12 +7,22 @@ public class jsjs extends ConsoleProgram {
 
 	public void run() {
 		String s = readLine("enter: ");
-		while(s != ""){
-		for(int i = 0; i < s.length(); i++){
-			
+		while (s != "") {
+			int amount = 0;
+			char X = ' ';
+			for (int i = 0; i < s.length(); i++) {
+				X = s.charAt(i);
+				for (int j = 0; j < s.length(); j++) {
+					if (s.charAt(j) == X) {
+						amount++;
+						String delete = "" + X;
+						String newS = s.replace(delete, "");
+						s = newS;
+					}
+				}
+			}
+			println("num of: " + X + " : " + amount);
 		}
-			
-		}	
 	}
 
 }
