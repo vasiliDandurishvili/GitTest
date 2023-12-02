063296fa255bf9780aa30d1c155bc89b9981a80a
S <s>
2023-12-02 20:02:35 +0400

    {"types":"C","runs":16}

--- jsjs.java
+++ jsjs.java
@@ -1,12 +1,47 @@
 import java.awt.event.MouseEvent;
 
 import acm.graphics.GLine;
+import acm.graphics.GOval;
 import acm.program.*;
+import acm.util.RandomGenerator;
 
-public class jsjs extends ConsoleProgram {
-
+public class jsjs extends GraphicsProgram {
+	private static final double MIN_RADIUS = 10;
+	private static final double MAX_RADIUS = 30;
+	private static final int NUM_CIRCLES = 20;
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	private double radius;
+	private GOval oval;
 	public void run() {
+		addMouseListeners();
+		for(int i = 0; i < NUM_CIRCLES; i++){
+			drawCircle();
+		}
+	}
+	private void drawCircle() {
+		radius = rgen.nextDouble(MIN_RADIUS, MAX_RADIUS);
+		oval = new GOval (2 * radius, 2 * radius);
+		double x = rgen.nextDouble(0, getWidth() - 2 * radius);
+		double y = rgen.nextDouble(0, getHeight() - 2 * radius);
+		oval.setFilled(true);
 		
+		add(oval, x, y);
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
