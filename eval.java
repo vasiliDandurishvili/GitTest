c2ca7ad0decb5c8714a4f84c6ffb92f1ccee5fb8
S <s>
2023-12-04 12:23:50 +0400

    {"types":"C","runs":35}

--- eval.java
+++ eval.java
@@ -1,4 +1,19 @@
+import java.util.StringTokenizer;
 
-public class eval {
+import acm.program.ConsoleProgram;
 
+public class eval extends ConsoleProgram{
+	public void run(){
+		String str = readLine();
+		ragac(str);
+	}
+
+	private int ragac(String str) {
+		StringTokenizer tok = new StringTokenizer(str, " ");
+		int k = Integer.parseInt(str);
+		
+		while(tok.hasMoreTokens()){
+			int 
+		}
+	}
 }
