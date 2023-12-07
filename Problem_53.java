4b1f7109e501ccb1242901650100173ab952f399
S <s>
2023-12-07 17:08:15 +0400

    {"types":"C","runs":41}

--- Problem_53.java
+++ Problem_53.java
@@ -1,4 +1,24 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
-public class Problem_53 {
+import acm.program.ConsoleProgram;
+
+/**53. დაწერეთ პროგრამა რომელიც ფაილიდან წაიკითხავს ტექტს და დათვლის შემდეგ
+სტატისტიკებს: სიმბოლოების რაოდენობა, სიტყვების რაოდენობა(სიტყვები სფეისებით
+გამოყოფილია ერთმანეთისგან), წინადადებების რაოდენობა. რამდენი წინდადება მთავრდება
+წერტილით, კითხვის ნიშნით და ძახილის ნიშნით?*/
+public class Problem_53 extends ConsoleProgram{
+	private static final String FILENAME = "statistics.txt";
+	public void run(){
+		try {
+			BufferedReader reader = new BufferedReader (new FileReader(FILENAME));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
 
 }
