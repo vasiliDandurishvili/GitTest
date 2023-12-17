0156b041e87ab659d0f454e1bae9335629b950e3
S <s>
2023-12-17 12:57:44 +0400

    {"types":"C","runs":46}

--- Problem_54.java
+++ Problem_54.java
@@ -1,4 +1,21 @@
+/**54. კონსოლიდან შეგყვავს n და შემდეგ n ცალი მთელი რიცხვი, იპოვეთ და დაბეჭდეთ ამ
+რიცხვებს შორის ორი მაქსიმალური რიცხვი.*/
+import java.util.Arrays;
 
-public class Problem_54 {
+import acm.program.ConsoleProgram;
+
+
+public class Problem_54 extends ConsoleProgram{
+	public void run(){
+		int n = readInt("enter n: ");
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++){
+			arr[i] = readInt("fill array: ");
+		}
+		Arrays.sort(arr);
+		
+		println("biggest: " + arr[n - 1]);
+		println("second biggest: " + arr[n - 2]);
+	}
 
 }
