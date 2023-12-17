0614153a740747ddc769c1bba60a1b693f845c11
S <s>
2023-12-17 12:29:10 +0400

    {"types":"C","runs":42}

--- Problem_53.java
+++ Problem_53.java
@@ -1,30 +1,69 @@
+//53. დაწერეთ პროგრამა რომელიც ფაილიდან წაიკითხავს ტექტს და 
+//დათვლის შემდეგ
+//სტატისტიკებს: სიმბოლოების რაოდენობა(alphanumeric), სიტყვების რაოდენობა
+//(სიტყვები სფეისებით
+//გამოყოფილია ერთმანეთისგან), წინადადებების რაოდენობა. 
+//		რამდენი წინდადება მთავრდება
+//წერტილით, კითხვის ნიშნით და ძახილის ნიშნით?
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 import acm.program.ConsoleProgram;
 
-/**53. დაწერეთ პროგრამა რომელიც ფაილიდან წაიკითხავს ტექტს და დათვლის შემდეგ
-სტატისტიკებს: სიმბოლოების რაოდენობა, სიტყვების რაოდენობა(სიტყვები სფეისებით
-გამოყოფილია ერთმანეთისგან), წინადადებების რაოდენობა. რამდენი წინდადება მთავრდება
-წერტილით, კითხვის ნიშნით და ძახილის ნიშნით?*/
 public class Problem_53 extends ConsoleProgram{
 	private static final String FILENAME = "statistics.txt";
-	public void run(){
+	public void run() {
 		try {
-			BufferedReader reader = new BufferedReader (new FileReader(FILENAME));
-			
+			BufferedReader reader = new BufferedReader(new FileReader(FILENAME));
 			String text = "";
-			while(true){
-				
+			while(true) {
+				String line = reader.readLine();
+				if (line == null) {
+					break;
+				}
+				text += line + "\n";
 			}
-		} 
-		catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
+			reader.close();
+			countStatistics(text);
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		
 	}
 
+	private void countStatistics(String text) {
+		int symbolCount = 0;
+		int wordCount = 0;
+		int dotCount = 0;
+		int questionCount = 0;
+		int exclamationCount = 0;
+		int sentenceCount = 0;
+
+		StringTokenizer tokenizer = new StringTokenizer(text, " ?!.\n", true);
+
+		while(tokenizer.hasMoreTokens()) {
+			String token = tokenizer.nextToken();
+			if(token.equals(".")) {
+				dotCount += 1;
+				sentenceCount += 1;
+			}else if(token.equals("!")) {
+				exclamationCount += 1;
+				sentenceCount += 1;
+			}else if(token.equals("?")) {
+				questionCount += 1;
+				sentenceCount += 1;
+			}else if(!token.equals(" ") && !token.equals("\n")) {
+				wordCount += 1;
+				symbolCount += token.length();
+			}
+		}
+
+		println("symbolCount " + symbolCount);
+		println("wordCount " + wordCount);
+		println("dotCount " + dotCount);
+		println("questionCount " + questionCount);
+		println("exclamationCount " + exclamationCount);
+		println("sentenceCount " + sentenceCount);		
+	}
 }
