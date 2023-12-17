76ec44ff132e29428a16957c383b7b5d2961028b
S <s>
2023-12-17 13:26:05 +0400

    {"types":"C","runs":52}

--- Problem_56.java
+++ Problem_56.java
@@ -1,15 +1,44 @@
+import java.util.Arrays;
+
 import acm.program.ConsoleProgram;
 /**56. კონსოლიდან შეგყვავს n და შემდეგ n ცალი მთელი რიცხვი, დაალაგეთ რიცხვები
 ზრდადობით და დაბეჭდეთ.*/
 public class Problem_56 extends ConsoleProgram{
 	public void run(){
 		int n = readInt("enter sum: ");
-		int [] a = new int[n];
+		int [] arr = new int[n];
 		for (int i = 0; i < n;i++){
-			a[i] = readInt("enter num: ");
+			arr[i] = readInt("enter num: ");
 		}
-		int[] digits = new int[10];
+		println(Arrays.toString(arr));
+		sort(arr);
+		println(Arrays.toString(arr));
 		 
+	}
+
+	private void sort(int[] arr) {
+		for(int i = 0 ; i < arr.length; i++){
+			int j = findMinIndex(arr , i);
+			swap(arr, i, j);
+		}
+		
+	}
+
+	private void swap(int[] arr, int i, int j) {
+		int temp = arr[j];
+		arr[j] = arr[i];
+		arr[i] = temp;
+		
+	}
+
+	private int findMinIndex(int[] arr, int i) {
+		int minIndex = i;
+		for (int j = i; j < arr.length; j++){
+			if(arr[j] < arr[minIndex]){
+				minIndex = j;
+			}
+		}
+		return minIndex;
 	}
 
 }
