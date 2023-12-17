e8a55962eb85352bfef132bc175f70446577148f
S <s>
2023-12-17 15:59:11 +0400

    {"types":"C","runs":66}

--- jsjs.java
+++ jsjs.java
@@ -18,7 +18,7 @@ public class jsjs extends ConsoleProgram {
 		
 		myList.add("hello");
 		myList.add("hi");
-		myList.set(2, "hey");
+		myList.set(1, "hey");
 		
 		for( int i = 0; i < myList.size(); i++){
 			println(myList.get(myList.size() - i -1 )); // print from back.
