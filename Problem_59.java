1573d2cebc07894652821f6cb8260fafa93e065b
S <s>
2023-12-23 12:52:52 +0400

    {"types":"C","runs":106}

--- Problem_59.java
+++ Problem_59.java
@@ -24,7 +24,42 @@ public class Problem_59 extends ConsoleProgram{
 			{0,0,1,1,1,1,0,0}, 
 			{0,0,1,1,1,1,0,0},
 		};
-	print(matrix1[0][1]);
-		
+		int sum = maxArea(matrix1);
+
+		println(sum);
+	}
+
+	private int maxArea(int[][] matrix1) {
+		int maxArea =0;
+		int nRows  = matrix1.length;
+		int nCols = matrix1[0].length;
+	
+		/**get all rect. take the rects starting coordinate.*/
+		for(int i1 = 0 ; i1 < nRows; i1++){
+			for(int j1 = 0; j1 < nCols; j1++){
+				/**take the rects last coordinate.*/
+				for(int i2 = 0; i2 < nRows; i2++){
+					for(int j2 = 0; j2 < nCols; j2++){
+						if(hasOnlyOnes(matrix1, i1, j1, i2, j2)){
+							int currArea = (i2 - i1 +1) * (j2 - j1 +1);
+							maxArea = Math.max(maxArea, currArea);
+						}
+					}
+				}
+				
+			}
+		}
+		return maxArea;
+	}
+
+	private boolean hasOnlyOnes(int[][] matrix1, int i1, int j1, int i2, int j2) {
+		for(int i = i1; i < i2; i++){
+			for(int j = j1; j < j2; j++){
+				if(matrix1[i][j] != 1){
+					return false;
+				}
+			}
+		}
+		return true;
 	}
 }
