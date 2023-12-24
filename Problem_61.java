2cde0487fb04192b6662ba764d9827b07a4f0228
S <s>
2023-12-24 18:47:42 +0400

    {"types":"C","runs":124}

--- Problem_61.java
+++ Problem_61.java
@@ -1,4 +1,19 @@
+import java.util.Arrays;
 
-public class Problem_61 {
+import acm.program.ConsoleProgram;
 
+/**61. კონსოლიდან შეგვყავს რიცხვი m და n, შემდეგ n ცალი რიცხვი. პროგრამამ უნდა იპოვოს
+შეყვანილი რიცხვებიდან წყვილი რომელთა ჯამი არის m.*/
+public class Problem_61 extends ConsoleProgram{
+	public void run(){
+		int m = readInt("enter m: ");
+		int n = readInt("enter n: ");
+		
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++){
+			arr [i] = readInt("enter num: ");
+		}
+		Arrays.toString(arr);
+		println(arr);
+	}
 }
