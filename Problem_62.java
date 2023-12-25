406586e36f6b6000ea9f31eac61278fda64ceb56
S <s>
2023-12-25 17:05:08 +0400

    {"types":"C","runs":0}

--- Problem_62.java
+++ Problem_62.java
@@ -1,4 +1,11 @@
+import acm.graphics.GObject;
+import acm.graphics.GRect;
+import acm.program.GraphicsProgram;
 
-public class Problem_62 {
+public class Problem_62 extends GraphicsProgram{
+	public void run(){
+		GObject r = new GRect (20,20);
+		add(r);
+	}
 
 }
