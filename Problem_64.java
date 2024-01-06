e96b877b0ae7fdf108003de37e370072119cc0dd
S <s>
2024-01-06 14:40:14 +0400

    {"types":"C","runs":16}

--- Problem_64.java
+++ Problem_64.java
@@ -1,4 +1,43 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+/**64. 63-ე ამოცანის გაგრძელება, დაბეჭდეთ თითოეული შეყვანილი სახელი და მის გასწვრივ
+მძიმით გამოყოფილი მეგობრების სია.*/
+import acm.program.ConsoleProgram;
 
-public class Problem_64 {
+public class Problem_64 extends ConsoleProgram {
+	private static final String SENTINEL = "";
+	Map<String, ArrayList<String>> myMap = new HashMap<>();
+	
+	public void run() {
 
+		while (true) {
+			String follower = readLine("enter follower: ");
+			if (follower.equals(SENTINEL)) {
+				break;
+			}
+
+			String person = readLine("enter person: ");
+
+			if (!myMap.containsKey(person)) {
+				myMap.put(person, new ArrayList<String>());
+			}
+			ArrayList<String> currFriends = myMap.get(person);
+			currFriends.add(follower);
+
+			// we don't need to call put again
+			// friendLists.put(person, currFriends);
+		}
+		for(String person: myMap.keySet()){
+			outputList(person);
+		}
+
+	}
+
+	private void outputList(String person) {
+		print(person + ": ");
+		for(int i = 0; i < myMap.get(person).size(); i++){
+			
+		}
+	}
 }
