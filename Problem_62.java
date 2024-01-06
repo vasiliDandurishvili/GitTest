5bbc3f89d48aaa5ec14a6c24490f8c2e48157920
S <s>
2024-01-06 11:52:49 +0400

    {"types":"C","runs":9}

--- Problem_62.java
+++ Problem_62.java
@@ -3,31 +3,27 @@ import java.util.HashMap;
 import acm.graphics.GObject;
 import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
-
-/**
- * 62. კონსოლიდან შეგვყავს სახელები, მანამ სანამ არ შევიყვანთ ცარიელ სტრინგს.
- * დაბეჭდეთ სახელი და მის გასწვრივ რაოდენობა რამდენჯერ შევიყვანეთ ეს სახელი.
- */
-public class Problem_62 extends GraphicsProgram {
+/**62. კონსოლიდან შეგვყავს სახელები, მანამ სანამ არ შევიყვანთ ცარიელ სტრინგს. დაბეჭდეთ
+სახელი და მის გასწვრივ რაოდენობა რამდენჯერ შევიყვანეთ ეს სახელი.*/
+public class Problem_62 extends GraphicsProgram{
 	private static final String SENTINEL = "";
-
-	public void run() {
-		HashMap<String, Integer> myMap = new HashMap<String, Integer>();
-
-		while(true) {
-			String name = readLine("Enter name: ");
-			if(name.equals(SENTINEL)) {
+	public void run(){
+		HashMap <String, Integer> myMap = new HashMap <String, Integer>();
+		while(true){
+			
+			String name = readLine("enter name: ");
+			
+			if(name.equals(SENTINEL)){
 				break;
 			}
-
-			if(!nameCounts.containsKey(name)) {
-				nameCounts.put(name, 0);
+			if(!myMap.containsKey(name)){
+				myMap.put(name, 0);
 			}
-
-			int newCount = nameCounts.get(name) + 1;
-			nameCounts.put(name, newCount);
+			
+			int nameNum = myMap.get(name) + 1;
+			
+			myMap.put(name, nameNum);
 		}
-
 		println(myMap);
 	}
 
