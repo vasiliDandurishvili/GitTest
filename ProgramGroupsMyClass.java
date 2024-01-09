d697be4769d271c5904f174ddc01be248f7620e2
S <s>
2024-01-09 17:22:03 +0400

    {"types":"C","runs":33}

--- ProgramGroupsMyClass.java
+++ ProgramGroupsMyClass.java
@@ -1,13 +1,15 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 public class ProgramGroupsMyClass {
 	private Map<String, Double> studentScores;
 	public ProgramGroupsMyClass (){
+		studentScores = new HashMap<String, Double>();
 		
 	}
 	
 	public ProgramGroupsMyClass (ArrayList<String> students){
-		
+		studentScores = new HashMap<String, Double>();studentScores.put
 	}
 }
