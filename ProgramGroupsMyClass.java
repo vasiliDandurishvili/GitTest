6b11ba7a8e8625e831ee2fa6594a7191e9f5cfe4
S <s>
2024-01-09 17:30:58 +0400

    {"types":"C","runs":33}

--- ProgramGroupsMyClass.java
+++ ProgramGroupsMyClass.java
@@ -5,14 +5,22 @@ import java.util.Map;
 public class ProgramGroupsMyClass {
 	private Map<String, Double> studentScores;
 	public ProgramGroupsMyClass (){
-		studentScores = new HashMap<String, Double>();
+		studentScores = new HashMap<String, Double>();//es pirvelia
 		
 	}
 	
-	public ProgramGroupsMyClass (ArrayList<String> students){
+	public ProgramGroupsMyClass (ArrayList<String> students){//es meore
 		studentScores = new HashMap<String, Double>();
 		for(String student: students){
 			studentScores.put(student, null);
 		}
 	}
+	public Double getScore(String student){
+		if(!studentScores.containsKey(student)){
+			System.out.println("no student: " + student);
+			return null;
+		}
+		return studentScores.get(student);
+	}
 }
+
