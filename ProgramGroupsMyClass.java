e62a6d9ab1e9e7d5a4c18a164a96b281b964ac89
S <s>
2024-01-09 17:17:41 +0400

    {"types":"C","runs":33}

--- ProgramGroupsMyClass.java
+++ ProgramGroupsMyClass.java
@@ -1,4 +1,13 @@
+import java.util.ArrayList;
+import java.util.Map;
 
 public class ProgramGroupsMyClass {
-
+	private Map<String, Double> studentScores;
+	public ProgramGroupsMyClass (){
+		
+	}
+	
+	public ProgramGroupsMyClass (ArrayList<String> students){
+		
+	}
 }
