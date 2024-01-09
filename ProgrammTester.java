700759b309b51386498032d041582a4576834af4
S <s>
2024-01-09 17:35:32 +0400

    {"types":"C","runs":33}

--- ProgrammTester.java
+++ ProgrammTester.java
@@ -1,4 +1,18 @@
+import java.util.ArrayList;
 
-public class ProgrammTester {
+import acm.program.ConsoleProgram;
+
+public class ProgrammTester extends ConsoleProgram{
+	
+	public void run(){
+		ArrayList<String> students = new ArrayList<>();
+		students.add("vako1");
+		students.add("vako2");
+//		ProgramGroupsMyClass classB = new ProgramGroupsMyClass ();
+		ProgramGroupsMyClass classB = new ProgramGroupsMyClass (students);
+		
+		println(classB.getScore("vako1"));
+		println(classB.getScore("vako133"));
+	}
 
 }
