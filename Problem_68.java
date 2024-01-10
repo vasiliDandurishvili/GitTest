5d6d6eb1280cb2c5f6222c3b5995ed6e89b4f846
S <s>
2024-01-10 15:41:27 +0400

    {"types":"C","runs":37}

--- Problem_68.java
+++ Problem_68.java
@@ -1,4 +1,18 @@
+import javax.swing.JButton;
+import javax.swing.JTextField;
 
-public class Problem_68 {
+import acm.program.GraphicsProgram;
+
+/**68. თქვენი ამოცანაა გრაფიკული პროგრამის საშუალებით გააკეთოთ ჩატი. ფანჯარაზე, SOUTH
+რეგიონზე უნდა გქონდეთ ერთი ტექსტფილდი და ერთი ღილაკი ტექტით Enter.
+ტექსტი მაინც უნდა გამოჩნდეს ეკრანის ბოლოში, შესაბამისად სულ პირველად შეყვანილი
+ტექსტი გაქრება ეკრანიდან. */
+public class Problem_68 extends GraphicsProgram{
+	private static final int Distance = 40;
+	private JTextField  textField;
+	private JButton button;
+	public void init(){
+		
+	}
 
 }
