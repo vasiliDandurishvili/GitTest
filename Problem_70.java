2b3a24d41809169778e2779d47d9b3c74e3fd5b7
S <s>
2024-01-11 09:25:22 +0400

    {"types":"C","runs":40}

--- Problem_70.java
+++ Problem_70.java
@@ -1,4 +1,15 @@
+import acm.graphics.GLine;
+import acm.program.GraphicsProgram;
 
-public class Problem_70 {
+public class Problem_70 extends GraphicsProgram{
+	private GLine line;
+	public void run(){
+		while(true){
+			removeAll();
+			for(int i = 0; i < 10; i++){
+				
+			}
+		}
+	}
 
 }
