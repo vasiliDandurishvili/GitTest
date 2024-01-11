cb11a83389259084d2a08ae2ada35ea27b353c97
S <s>
2024-01-11 21:12:31 +0400

    {"types":"C","runs":43}

--- Problem_68.java
+++ Problem_68.java
@@ -19,37 +19,13 @@ public class Problem_68 extends GraphicsProgram {
 	private double x = DISTANCE;
 	private double y = DISTANCE;
 
-	public void init() {
-		textField = new JTextField(20);
+	public void init(){
+		textField = new JTextField (20);
 		add(textField, SOUTH);
-
 		textField.addActionListener(this);
-
-		button = new JButton("enter");
-		add(button, SOUTH);
-
-		addActionListeners();
+		
+		
 	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		println("here");
-		if (e.getSource() == textField) {
-			if (e.getSource() == textField || e.getSource() == button) {
-				String text = textField.getText();
-				println(text);
-				displayMessage(text);
-				textField.setText("");
-			}
-		}
-	}
-
-	private void displayMessage(String text) {
-		if (text == null || text.equals("")) {
-			return;
-		}
-		GLabel message = new GLabel(text);
-		add(message, x, y);
-		y += DISTANCE;
-	}
+	
+	
 }
