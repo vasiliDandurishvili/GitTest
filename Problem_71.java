3d697ef6d26de887a18d0864b61798ec8a067ba6
S <s>
2024-01-12 16:15:52 +0400

    {"types":"C","runs":62}

--- Problem_71.java
+++ Problem_71.java
@@ -1,4 +1,13 @@
+import acm.program.ConsoleProgram;
 
-public class Problem_71 {
+public class Problem_71 extends ConsoleProgram{
+	public void run(){
+		String a = readLine();
+		char b = a.charAt(0);
+		b = Character.toUpperCase(b);
+		
+		println(b);
+		
+	}
 
 }
