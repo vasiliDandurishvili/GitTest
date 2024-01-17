7045f689b269e2294c7607780ae370cd9b089818
S <s>
2024-01-17 11:07:13 +0400

    {"types":"C","runs":13}

--- Final_2022_1.java
+++ Final_2022_1.java
@@ -1,4 +1,22 @@
+import acm.program.ConsoleProgram;
 
-public class Final_2022_1 {
+public class Final_2022_1 extends ConsoleProgram{
+	public void run(){
+		String line = readLine();
+		
+		String compressed = manipString(line);
+		
+		if(line.length() > compressed.length()){
+			println(compressed);
+		}else{
+			println(line);
+		}
+		
+	}
+
+	private String manipString(String line) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
