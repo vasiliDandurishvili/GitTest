b8040db67a4239bd68ba6cdeb975ab20bb2ed542
S <s>
2024-01-17 11:23:17 +0400

    {"types":"C","runs":14}

--- Final_2022_1.java
+++ Final_2022_1.java
@@ -13,13 +13,20 @@ public class Final_2022_1 extends ConsoleProgram{
 		}
 		
 	}
-
+// aaabbbbbccd
 	private String manipString(String line) {
 		int sames = 1;
 		String result = "";
 		for(int i = 0; i < line.length(); i++){
 			if(i < line.length() -1 && line.charAt(i) != line.charAt(i+1)){
+				/**bolos ar tvlis.*/
 				String psLine = line.substring(0, i + 1);
+				result = line.charAt(0) + sames + "";
+				line = line.substring(i + 1);
+				sames = 1;
+				
+			}else{
+				sames += 1;
 			}
 			
 			
