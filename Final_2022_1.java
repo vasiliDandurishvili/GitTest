315ee0ed794222898577424de9e258d66f0dcd57
S <s>
2024-01-17 11:48:29 +0400

    {"types":"C","runs":34}

--- Final_2022_1.java
+++ Final_2022_1.java
@@ -20,23 +20,7 @@ public class Final_2022_1 extends ConsoleProgram{
 		if(line.length() == 1){
 			return line.charAt(0) + "" + 1;
 		}
-		int sames = 1;
-		String result = "";
-		for(int i = 0; i < line.length(); i++){
-			
-			if(i < line.length() - 1 && line.charAt(i) != line.charAt(i+1)){
-				/**bolos ar tvlis.*/
-				String psLine = line.substring(0, i + 1);
-				result += line.charAt(0) +  "" + sames;
-				line = line.substring(i + 1);
-				sames = 1;
-				
-			}else{
-				sames += 1;
-			}
-			
-			
-		}
+		
 		
 		return result;
 	}
