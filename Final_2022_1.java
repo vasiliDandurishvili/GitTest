370f68bb7cc9d2a0bc5b322a8c19da4bb2bdf5b3
S <s>
2024-01-17 12:10:41 +0400

    {"types":"C","runs":42}

--- Final_2022_1.java
+++ Final_2022_1.java
@@ -3,41 +3,47 @@ import acm.program.ConsoleProgram;
 
 //aaabbbbbccd -> a3b4c2d1 
 
-public class Final_2022_1 extends ConsoleProgram {
-    public void run() {
-        String line = readLine();
-        String compressed = manipString(line);
+public class Final_2022_1 extends ConsoleProgram{
+	public void run(){
+		String line = readLine();
+		
+		String compressed = manipString(line);
+		
+		if(line.length() > compressed.length()){
+			println(compressed);
+		}else{
+			println(line);
+		}
+		
+	}
+	private String manipString(String line) {
+		if(line.length() == 1){
+			return line.charAt(0) + "" + 1;
+		}
+		int sames = 1;
+		String result = "";
+		for(int i = 0; i <= line.length() - 1; i++){
+			if(i != line.length() - 1 && line.charAt(i) != line.charAt(+1)){
+				String psLine = line.substring(0, i + 1);
+				result += line.charAt(0) + "" + sames;
+				line = line.substring(i + 1);
+				sames = 1;
+				i = -1;
+			}else{
+				sames += 1;
+			}
+			
+		}
+		/**if last digit is different from before line should be 1 size.*/
+		if(line.length() == 1){
+			result += line.charAt(0) + "" + 1; 
+		}/**if last digits are same add to result*/
+		else{
+			result += line.charAt(0) + "" + line.length();
+		}
+		
+		
+		return result;
+	}
 
-        if (line.length() > compressed.length()) {
-            println(compressed);
-        } else {
-            println(line);
-        }
-    }
-
-    private String manipString(String line) {
-        if (line.length() == 1) {
-            return line.charAt(0) + "" + 1;
-        }
-        int sames = 1;
-        String result = "";
-
-        for (int i = 0; i < line.length() - 1; i++) {
-            if (line.charAt(i) != line.charAt(i + 1)) {
-                result += line.charAt(i) + "" + sames;
-                sames = 1;
-            } else {
-                sames += 1;
-            }
-        }
-
-        // Handle the last character in the string
-        if (line.charAt(line.length() - 1) != line.charAt(line.length() - 2)) {
-            result += line.charAt(line.length() - 1) + "" + 1;
-        } else {
-            result += line.charAt(line.length() - 1) + "" + sames;
-        }
-
-        return result;
-    }
 }
