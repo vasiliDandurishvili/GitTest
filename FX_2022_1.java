44d95f38e41a02d2ab71ddeb225fd25b2f7a2503
S <s>
2024-01-18 22:49:28 +0400

    {"types":"C","runs":55}

--- FX_2022_1.java
+++ FX_2022_1.java
@@ -1,4 +1,23 @@
+import acm.program.ConsoleProgram;
 
-public class FX_2022_1 {
+public class FX_2022_1 extends ConsoleProgram{
+	public void run(){
+		int[] arr = new int [6];
+		for(int i = 0; i < arr.length; i++){
+			arr[i] = readInt();
+		}
+		
+		String sum = intToString(arr);
+		
+		println(sum);
+	}
 
+	private String intToString(int[] arr) {
+		String result = "";
+		for(int i = 0; i < arr.length; i++){
+			
+		}
+		
+		return result;
+	}
 }
