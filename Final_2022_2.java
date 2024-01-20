5c29e4245a07003add41ad2a8f8e9dde616c0619
S <s>
2024-01-20 13:52:37 +0400

    {"types":"C","runs":82}

--- Final_2022_2.java
+++ Final_2022_2.java
@@ -1,4 +1,70 @@
+import acm.program.ConsoleProgram;
+import acm.util.RandomGenerator;
 
-public class Final_2022_2 {
+/**matricis ganuleba.*/
+public class Final_2022_2 extends ConsoleProgram{
+	public void run(){
+		
+	}
+	private void zero(int[][] matrix){
+		int temp = findNumThatIsNotInMtx(matrix);
+		
+		changeMtxWithTmps(matrix,temp );
+		changeTmpsWithZeros(matrix,temp);
+	}
+	private int findNumThatIsNotInMtx(int[][] matrix) {
+		RandomGenerator rgen = RandomGenerator.getInstance();
+		while(true){
+			int tmp = rgen.nextInt();
+			if(!contains(matrix,tmp)){
+				return tmp;
+			}
+		}
 
+	}
+	private boolean contains(int[][] matrix, int tmp) {
+		int row = matrix.length;
+		int col = matrix[0].length;
+		for(int i = 0; i < row; i++){
+			for(int j = 0; j < col; j++){
+				if(matrix[i][j] == tmp){
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	private void changeTmpsWithZeros(int[][] matrix, int tmp) {
+		int row = matrix.length;
+		int col = matrix[0].length;
+		for(int i = 0; i < row; i++){
+			for(int j = 0; j < col; j++){
+				if(matrix[i][j] == tmp){
+					matrix[i][j] == 0;
+				}
+			}
+		}
+		
+	}
+	private void changeMtxWithTmps(int[][] matrix, int tmp) {
+		int row = matrix.length;
+		int col = matrix[0].length;
+		for(int i = 0; i < row; i++){
+			for(int j = 0; j < col; j++){
+				if(matrix[i][j] == 0){
+					changeColWithTmps(matrix, j, tmp);
+					changerowWithTmps(matrix, i, tmp);
+				}
+			}
+		}
+	}
+	private void changerowWithTmps(int[][] matrix, int i, int tmp) {
+		int row = matrix.length;
+		int col = matrix[0].length;	
+		
+	}
+	private void changeColWithTmps(int[][] matrix, int j, int tmp) {
+		int row = matrix.length;
+		int col = matrix[0].length;		
+	}
 }
