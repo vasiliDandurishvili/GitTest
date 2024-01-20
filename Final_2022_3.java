262ddb840f4634e7596f2d14e1b9d4c1d7e14a43
S <s>
2024-01-20 14:15:16 +0400

    {"types":"C","runs":82}

--- Final_2022_3.java
+++ Final_2022_3.java
@@ -1,4 +1,33 @@
+import java.util.ArrayList;
 
-public class Final_2022_3 {
+import acm.program.ConsoleProgram;
 
+public class Final_2022_3 extends ConsoleProgram{
+	public void run(){
+		
+	}
+	private boolean sorted(ArrayList<Integer> a){
+		
+		for(int i = 0; i < a.size();i++){
+			for(int j = i + 1; j < a.size();j++ ){
+				swap(a, i, j);
+				if(sortedWithReverse(a)){
+					return true;
+				}
+				/**ukanac isev iseti gavxadot tu ar gamovida.*/
+				swap(a,i,j);
+			}
+		}
+		return false;
+	}
+	private boolean sortedWithReverse(ArrayList<Integer> a) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	private void swap(ArrayList<Integer> a, int i, int j) {
+		int temp = a.get(i);
+		a.set(i, j);
+		a.set(j, temp);
+		
+	}
 }
