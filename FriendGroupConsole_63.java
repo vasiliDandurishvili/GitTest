614a348f32383a1700e12a53d2649fe087064201
S <s>
2024-01-23 15:03:13 +0400

    {"types":"C","runs":16}

--- FriendGroupConsole_63.java
+++ FriendGroupConsole_63.java
@@ -1,7 +1,17 @@
 import acm.program.ConsoleProgram;
 
 public class FriendGroupConsole_63 extends ConsoleProgram {
+	private static final String SENTINEL = "";
 	public void run() {
-
+		FriendGroup myFriendGroup = new FriendGroup();
+		
+		while(true){
+			String friend = readLine("enter friend: ");
+			if(friend.equals(SENTINEL)){
+				break;
+			}
+			String person = readLine("enter person: ");
+			myFriendGroup.addFriendship(person, friend);
+		}
 	}
 }
