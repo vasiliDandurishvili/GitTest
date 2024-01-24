3527da837e19ede77b953fdc71b4b00d1c217454
S <s>
2024-01-24 19:53:48 +0400

    {"types":"C","runs":23}

--- Mamashviloba.java
+++ Mamashviloba.java
@@ -1,4 +1,15 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
-public class Mamashviloba {
+import acm.program.ConsoleProgram;
 
+public class Mamashviloba extends ConsoleProgram{
+	private Map <String, ArrayList<String>> sonDad = new HashMap<>();
+	private Map <String, ArrayList<String>> dadSon = new HashMap<>();
+	String name;
+	public void run(){
+		name = readLine("name: ");
+	}
+	private ArrayList<String> getCousins()
 }
