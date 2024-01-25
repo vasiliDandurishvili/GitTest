be092cab152c33a88425ae65b5e7a5ba9631f2c8
S <s>
2024-01-25 20:08:49 +0400

    {"types":"C","runs":52}

--- bank_account.java
+++ bank_account.java
@@ -1,37 +1,37 @@
 import acm.program.ConsoleProgram;
 
-public class bank_account extends ConsoleProgram{
+public class bank_account extends ConsoleProgram {
 	double money;
-	public void run(){
+
+	public void run() {
 		money = 1000;
-		String line = readLine("balance or transaction?");
-		if(line.equals("check balance")){
-			bandAccount(money);
-		}else{
-			transactions(money);
+		while (true) {
+			String line = readLine("balance or transaction?");
+			if (line.equals("check balance")) {
+				bandAccount(money);
+			} else {
+				transactions(money);
+			}
 		}
 	}
 
-	
-	
-	private void bandAccount(double money){
+	private void bandAccount(double money) {
 		String line2 = readLine("give money: ");
-		if(line2.equals("balance")){
+		if (line2.equals("balance")) {
 			println("balance: " + money);
-		}else if(line2.equals("deposit")){
+		} else if (line2.equals("deposit")) {
 			int dep = readInt("deposit: ");
 			money += dep;
 			println("new balance: " + money);
-		}else if(line2.equals("withdraw")){
+		} else if (line2.equals("withdraw")) {
 			int with = readInt("withdraw: ");
 			money -= with;
-			println("new balance: " + money);	
+			println("new balance: " + money);
 		}
-			
-		
+
 	}
-	
-	private void transactions(double money){
-		
+
+	private void transactions(double money) {
+
 	}
 }
