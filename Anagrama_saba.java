d58c6ea03e23a4473bf34d40596556d479689b23
S <s>
2024-01-25 19:41:25 +0400

    {"types":"C","runs":50}

--- Anagrama_saba.java
+++ Anagrama_saba.java
@@ -1,4 +1,11 @@
+import acm.program.ConsoleProgram;
 
-public class Anagrama_saba {
+//ori stringi shemodis 
+public class Anagrama_saba extends ConsoleProgram{
+	public void run(){
+		String line1 = readLine();
+		String line2 = readLine();
+		
+	}
 
 }
