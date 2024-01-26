456cc6cac212a68d9a970131505719856c42bb0c
S <s>
2024-01-26 16:53:49 +0400

    {"types":"C","runs":63}

--- Final_joker.java
+++ Final_joker.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 import acm.program.ConsoleProgram;
 import acm.util.RandomGenerator;
 
@@ -9,6 +11,22 @@ public class Final_joker extends ConsoleProgram{
 	private double joker(int simulations) {
 		RandomGenerator rgen = RandomGenerator.getInstance();
 		int counter = 0;
+		for(int j = 0; j < simulations;j++){
+			ArrayList<Integer>[] c = new ArrayList[4];
+			for(int k = 0; k < 4; k++){
+				c[k] = new ArrayList<>();
+			}
+			ArrayList<Integer> cards = new ArrayList<Integer>();
+			for(int i = 0; i < 36; i++){
+				cards.add(i);
+			}
+			for(int i = 0; i < 36; i++){
+				c[i % 4].add(cards.remove(rgen.nextInt(cards.size())));
+			}
+			if(c[0].contains(0) && c[0].contains(1)){
+				counter++;
+			}
+		}
 		
 		return (double)counter / simulations;
 	}
