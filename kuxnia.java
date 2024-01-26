49a5b9578666cf5a1e59f96dda9811b774cf941d
S <s>
2024-01-26 12:18:50 +0400

    {"types":"C","runs":59}

--- kuxnia.java
+++ kuxnia.java
@@ -1,4 +1,28 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class kuxnia {
+	
+
+	public ArrayList<String> kitchen(HashMap<String, ArrayList<String>> receipts,ArrayList<String> fridge) {
+		ArrayList<String> result = new ArrayList<String>();
+		while(true){
+			ArrayList<String> dishes = new ArrayList<String>();
+			for(String dish: receipts.keySet()){
+				if(result.contains(dish)){
+					continue;
+				}
+				if(fridge.contains(dish)){
+					dishes.add(dish);
+					continue;
+				}
+			}
+		
+			if(true){
+				break;
+			}
+		}
+		return result;
+	}
 
 }
