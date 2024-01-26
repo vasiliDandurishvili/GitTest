7bb887f247a689d407f787f4a35913b401d166d7
S <s>
2024-01-26 16:31:19 +0400

    {"types":"C","runs":63}

--- Final_joker.java
+++ Final_joker.java
@@ -1,4 +1,16 @@
+import acm.program.ConsoleProgram;
+import acm.util.RandomGenerator;
 
-public class Final_joker {
+public class Final_joker extends ConsoleProgram{
+	public void run(){
+		println(joker(10000));
+	}
+
+	private double joker(int simulations) {
+		RandomGenerator rgen = RandomGenerator.getInstance();
+		int counter = 0;
+		
+		return (double)counter / simulations;
+	}
 
 }
